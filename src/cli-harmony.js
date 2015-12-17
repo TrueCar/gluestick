82f2d4b774cd4830ff72e829aba8756971bd8f91
Todd Williams <toddsurfs@icloud.com>
2015-12-17 15:28:45 -0800

    fix ignore function

--- src/cli-harmony.js
+++ src/cli-harmony.js
@@ -4,12 +4,8 @@
 require("babel-core/register")({
     stage: 0,
     ignore: function(filename) {
-        if (!/node_modules/.test(__dirname)) {
-            return !/\/gluestick\/src\//.test(filename);
-        }
-
         // ignore node modules in the current directory
-        if (filename.indexOf(process.cwd()) === 0) {
+        if (!/node_modules/.test(__dirname) || filename.indexOf(process.cwd()) === 0) {
             return /node_modules/.test(filename);
         }
 
