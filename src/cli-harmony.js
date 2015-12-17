5ef3b159ecf7fa8317b32e384185b1aa80d2e6a8
Todd Williams <toddsurfs@icloud.com>
2015-12-17 15:15:15 -0800

    update ignore so that `sudo npm link` will work

--- src/cli-harmony.js
+++ src/cli-harmony.js
@@ -4,6 +4,10 @@
 require("babel-core/register")({
     stage: 0,
     ignore: function(filename) {
+        if (!/node_modules/.test(__dirname)) {
+            return !/\/gluestick\/src\//.test(filename);
+        }
+
         // ignore node modules in the current directory
         if (filename.indexOf(process.cwd()) === 0) {
             return /node_modules/.test(filename);
