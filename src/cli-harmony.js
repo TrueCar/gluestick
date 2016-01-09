5514cb9860c42774ee689c2a6fcc2b036139ca40
Todd Williams <toddsurfs@icloud.com>
2016-01-08 16:36:23 -0800

    Fix npm start
    When you run `npm start` it will run `gluestick start` but it doesn't
    run from /usr/local/bin it actually runs it from the project's
    node_moduls/.bin/gluestick location. For this reason we cannot ignore
    the `cli.js` file or we wont be able to use ES6 in our command line
    interface.  do not skip the babel hook on the cli file

--- src/cli-harmony.js
+++ src/cli-harmony.js
@@ -6,6 +6,15 @@ require("babel-core/register")({
     ignore: function(filename) {
         // ignore node modules in the current directory
         if (!/node_modules/.test(__dirname) || filename.indexOf(process.cwd()) === 0) {
+            // When you run `npm start` it will run `gluestick start` but it
+            // doesn't run from /usr/local/bin it actually runs it from the
+            // project's node_moduls/.bin/gluestick location. For this reason
+            // we cannot ignore the `cli.js` file or we wont be able to use ES6
+            // in our command line interface.
+            // do not skip the babel hook on the cli file
+            if (/node_modules\/gluestick\/src\/cli.js/.test(filename)) return false;
+
+            // do skip the rest of the files in node_modules
             return /node_modules/.test(filename);
         }
 
