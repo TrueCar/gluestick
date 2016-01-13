4efe716ad29ede260fdc4408d433111e1277ab80
Todd Williams <toddsurfs@icloud.com>
2016-01-12 21:04:16 -0800

    Fix shared `store` instance bug
    The way the store was being created and imported was actually creating a
    single store instance that was shared on the server. This update ensures
    that it generates a new store on each new web request.

--- new/src/config/.store.js
+++ new/src/config/.store.js
@@ -1,5 +1,7 @@
 /** DO NOT MODIFY **/
 // The following lines create the store and properly sets up hot module replacement for reducers
 import { createStore } from "gluestick";
-export default createStore(() => require("../reducers"), (cb) => module.hot && module.hot.accept("../reducers", cb));
+export default function () {
+    return createStore(() => require("../reducers"), (cb) => module.hot && module.hot.accept("../reducers", cb));
+}
 
