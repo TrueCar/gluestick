18366b9fa9358ddff3ade3a1cc562b26b93c6b06
Todd Williams <toddsurfs@icloud.com>
2016-01-20 10:16:56 -0800

    update to work on windows

--- src/cli-harmony.js
+++ src/cli-harmony.js
@@ -5,8 +5,11 @@ require("babel-core/register")({
   stage: 0,
   ignore: function(filename) {
     var node_modules = "node_modules";
-    var slash = process.platform === "win32" ? "\\" : "/";
-    var gluestick_folder = [node_modules, "gluestick", "src"].join(slash);
+    // We don't use path.join because the filename slashes use the Unix style
+    // forward slashes on unix and on windows but path.join will give us the
+    // filenames based on the system. This way we force forward slashs and it
+    // works on Windows and Unix
+    var gluestick_folder = [node_modules, "gluestick", "src"].join("/");
 
     // Make sure babel does not ignore 
     if (filename.includes(gluestick_folder)) return false;
