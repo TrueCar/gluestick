8678592ce7c08d3a39ed56ba18ee17703779fe93
Todd Williams <toddsurfs@icloud.com>
2016-01-20 18:47:37 -0800

    update exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 export Root from "./containers/Root";
 export createStore from "./lib/createStore";
+export prepareRoutesWithTransitionHooks from "./lib/prepareRoutesWithTransitionHooks";
+export * from "./lib/route-helper";
+export * from "./lib/constants";
 export * from "./lib/constants";
 
