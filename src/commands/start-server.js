1b9d2b85c7e538cca06faa3e8f5275b8f25f1ba4
Todd Williams <toddsurfs@icloud.com>
2016-02-09 19:05:51 -0800

    Stop tailing log file, use pm2 logs method

--- src/commands/start-server.js
+++ src/commands/start-server.js
@@ -1,8 +1,8 @@
 import path from "path";
 import process from "process";
+import { spawn } from "child_process";
 import pm2 from "pm2";
 import sha1 from "sha1";
-import tail from "tail";
 import fs from "fs";
 
 // The number of server side rendering instances to run. This can be set with
@@ -34,8 +34,6 @@ module.exports = function startServer () {
       environment_name: process.env.NODE_ENV,
       no_autorestart: false,
       merge_logs: true,
-      error_file: "output.log",
-      out_file: "output.log",
       watch: process.env.NODE_ENV !== "production"
     }, (error, a) => {
       if (error) {
@@ -43,14 +41,8 @@ module.exports = function startServer () {
         pm2.disconnect();
       }
 
-      // Create the log file if it doesn't exist
-      const logPath = "output.log";
-      fs.closeSync(fs.openSync(logPath, "a"));
-
-      const logTail = new tail.Tail(logPath);
-      logTail.on("line", function (data) {
-        console.log(data);
-      });
+      // start showing the logs
+      spawn("pm2", ["logs", name, "--lines", 0], {stdio: "inherit"});
     });
 
     /**
