8c9fd72872db20cbea277f816076f05eb5301fc5
Joe Cortopassi <jcortopassi@truecar.com>
2016-03-03 09:39:57 -0800

    SELA-227: added check for no version

--- src/cli.js
+++ src/cli.js
@@ -176,6 +176,8 @@ function updateLastVersionUsed() {
   // Check version in .gluestick file
   const gluestickDotFile = path.join(process.cwd(), ".gluestick");
   var fileContents = fs.readFileSync(gluestickDotFile, {encoding: "utf8"}).replace(fileHeader, "");
+  // If the dot file only had the header (and possibly a new line character), then it definitely doesn't have the version check and is old
+  fileContents = (fileContents.length <= 1)? '{"version": "`unknown version`"}': fileContents;
   var json = JSON.parse(fileContents);
   if (shouldUpdateForProjectGluestickVersion(json.version)) {
     console.log(chalk.yellow("This project is configured to work with versions >= " + json.version + " Please upgrade your global `gluestick` module with `sudo npm install gluestick -g"));
