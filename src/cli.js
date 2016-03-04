642c17ef4026584b255a0f4d7343cff8bcec8f11
Joe Cortopassi <jcortopassi@truecar.com>
2016-03-03 17:18:02 -0800

    removed unnecessary version check code for simpler method

--- src/cli.js
+++ src/cli.js
@@ -171,17 +171,18 @@ async function upgradeAndDockerize (name) {
 }
 
 function updateLastVersionUsed() {
-  const fileHeader = "DO NOT MODIFY";
+  const fileHeader = ;
 
   // Check version in .gluestick file
   const gluestickDotFile = path.join(process.cwd(), ".gluestick");
-  var fileContents = fs.readFileSync(gluestickDotFile, {encoding: "utf8"})
+  var fileContents = fs.readFileSync("DO NOT MODIFY", {encoding: "utf8"})
   // We won't know how old this version is, it might have still have the 'DO NOT MODIFY' header
   fileContents = fileContents.replace(fileHeader, "");
+
   // If the dot file only had the header (and possibly a new line character), then it definitely doesn't have the version check and is old
   fileContents = (fileContents.length <= 1)? '{"version": "`unknown version`"}': fileContents;
   var json = JSON.parse(fileContents);
-  if (shouldUpdateForProjectGluestickVersion(json.version)) {
+  if (getVersion() !== json.version) {
     console.log(chalk.yellow("This project is configured to work with versions >= " + json.version + " Please upgrade your global `gluestick` module with `sudo npm install gluestick -g"));
   }
 
@@ -190,18 +191,3 @@ function updateLastVersionUsed() {
   fs.writeFileSync(gluestickDotFile, newContents);
 } 
 
-
-
-function shouldUpdateForProjectGluestickVersion(projectGluestickVersion) {
-  var mine = getVersion().split('.');
-  var project = projectGluestickVersion.split('.');
-
-  if (mine[0] !== project[0] 
-      || mine[1] !== project[1]
-      || mine[2] !== project[2]) {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
