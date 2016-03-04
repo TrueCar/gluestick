4d7c908c72674552706ec1492d49586c4c434933
Joe Cortopassi <jcortopassi@truecar.com>
2016-03-03 17:14:04 -0800

    always be able to deal with old file header, but never re-add it

--- src/cli.js
+++ src/cli.js
@@ -175,7 +175,9 @@ function updateLastVersionUsed() {
 
   // Check version in .gluestick file
   const gluestickDotFile = path.join(process.cwd(), ".gluestick");
-  var fileContents = fs.readFileSync(gluestickDotFile, {encoding: "utf8"}).replace(fileHeader, "");
+  var fileContents = fs.readFileSync(gluestickDotFile, {encoding: "utf8"})
+  // We won't know how old this version is, it might have still have the 'DO NOT MODIFY' header
+  fileContents = fileContents.replace(fileHeader, "");
   // If the dot file only had the header (and possibly a new line character), then it definitely doesn't have the version check and is old
   fileContents = (fileContents.length <= 1)? '{"version": "`unknown version`"}': fileContents;
   var json = JSON.parse(fileContents);
@@ -183,9 +185,8 @@ function updateLastVersionUsed() {
     console.log(chalk.yellow("This project is configured to work with versions >= " + json.version + " Please upgrade your global `gluestick` module with `sudo npm install gluestick -g"));
   }
 
-  // update version in dot file
-  var newContents = fileHeader + "\n";
-  newContents += JSON.stringify({version: getVersion()});
+  // update version in dot file. No longer want the 'Do Not Modify' text
+  const newContents = JSON.stringify({version: getVersion()});
   fs.writeFileSync(gluestickDotFile, newContents);
 } 
 
