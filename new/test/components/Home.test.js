1a9b627cbf78d3ca5fd64e91c505939ea727def0
Christine Brass <cybrass@gmail.com>
2016-03-07 21:13:48 -0500

    More linting

--- new/test/components/Home.test.js
+++ new/test/components/Home.test.js
@@ -2,9 +2,9 @@ import Home from "components/Home";
 import { shallow } from "enzyme";
 
 describe("components/Home", () => {
-    it("should render without an issue", () => {
-        const subject = <Home />;
-        expect(shallow(subject)).to.exist;
-    });
+  it("should render without an issue", () => {
+    const subject = <Home />;
+    expect(shallow(subject)).to.exist;
+  });
 });
 
