a5d1587e7c5e46d3a36d06426e93be8f1e0fd995
Joe Cortopassi <jcortopassi@truecar.com>
2016-03-07 16:12:19 -0800

    fixed directory errors

--- src/lib/updateVersion.js
+++ src/lib/updateVersion.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
 const path = require("path");
-const getVersion = require("./lib/getVersion");
+const getVersion = require("./getVersion.js");
+const chalk = require("chalk");
 
 module.exports = function updateLastVersionUsed() {
   // Check version in .gluestick file
   const gluestickDotFile = path.join(process.cwd(), ".gluestick");
   var fileContents = fs.readFileSync(gluestickDotFile, {encoding: "utf8"})
   // We won't know how old this version is, it might have still have the 'DO NOT MODIFY' header
-  fileContents = fileContents.replace(fileHeader, "");
+  fileContents = fileContents.replace("DO NOT MODIFY", "");
 
   // If the dot file only had the header (and possibly a new line character), then it definitely doesn't have the version check and is old
   fileContents = (fileContents.length <= 1)? '{"version": "`unknown version`"}': fileContents;
