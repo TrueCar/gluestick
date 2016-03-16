527447a4a9ccfa29b6e25e3959b06326adf27808
Jesse Mandel <jmandel@truecar.com>
2016-03-16 11:04:23 -0700

    -Created a base gluestick docker image that has a majority of the dependencies pre-installed to speed up dockerize command for apps -Added postpublish script to create the docker image with each npm publish automatically

--- src/commands/dockerize.js
+++ src/commands/dockerize.js
@@ -5,6 +5,6 @@ import fs from "fs";
 
 module.exports = function (name) {
   const { version: gluestickVersion } = JSON.parse(fs.readFileSync(path.join(__dirname, "../../package.json"), "utf-8"));
-  spawn("docker", ["build", "-t", name, "--build-arg", `GLUESTICK_VERSION=${gluestickVersion}`, "-f", path.join(process.cwd(), "src", "config", ".Dockerfile"), process.cwd()], {stdio: "inherit"});
+  spawn("docker", ["build", "-t", name, "-f", path.join(process.cwd(), "src", "config", ".Dockerfile"), process.cwd()], {stdio: "inherit"});
 };
 
