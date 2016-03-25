870140b07f6613ce008484cddcee0027c5c10f98
Joe Cortopassi <joe@joecortopassi.com>
2016-03-25 12:29:11 -0700

    Updated the auto-upgrade script to fix issue where .dockerignore was being placed incorrectly. Also broke it down for easier testing and readability

--- src/autoUpgrade/index.js
+++ src/autoUpgrade/index.js
@@ -9,69 +9,84 @@ import { highlight, filename } from "../lib/logsColorScheme";
 
 const CWD = process.cwd();
 
-/**
+
+/*
  * Let the user know that we are updating the file and copy the contents over.
  *
  * @param {String} name the name of the file
  * @param {String} data the data for the new file
+ *
  */
-function replaceFile (name, data) {
-  const filePath = getCurrentFilePath(name);
+function replaceFile (path, name, data) {
+  const filePath = path.join(path, name);
   logger.info(`${highlight(name)} file out of date.`);
   logger.success(`Updating ${filename(filePath)}`);
 
   fs.writeFileSync(filePath, data);
 }
 
-/**
- * Get the path to the file in the current project's config folder.
+
+/*
+ * template file getter
+ * @param {String} filePath normal file path to retrieve from template directory
+ * @return {String} file contents
  *
- * @param {String} name the file name we are looking for
  */
-function getCurrentFilePath (name) {
-  return path.join(CWD, "src", "config", name);
+function getTemplateFileFromFile(filePath) {
+  return fs.readFileSync(path.join(__dirname, "..", "..", "new", filePath), "utf8");
 }
+
+
+/*
+ * @param {String} filePath
+ * @return {Bool} has the file changed
+ */
+function hasFileChanged(filePath) {
+  const currentFile = fs.readFileSync(path.join(__dirname, "..", "..", filePath));
+  const newFile = path.join(CWD, filePath);
+  return (sha1(currentFile) !== sha1(newFile));
+}
+
+/*
+ * Composer for filechecks
+ * @param {Function} fileTest
+ * @param {Function} replacement
+ * @return {Function} fileReplacer
+ */
+function testAndReplace(fileTest) {
+  return function (filePath) {
+    if (!fileTest(filePath)){
+      const newFile = getTemplateFileFromFile(filePath);
+      const path = path.join(CWD, path.parse(filePath).dir);
+      const file = path.parse(filePath).base;
+      replaceFile(path, file, newFile);
+    }
+  };
+}
+
+
+
 
 module.exports = async function () {
   await updatePackage();
 
-  // Check for certain files that we've added to new Gluestick applications. If those files don't exist, add them
-  // for the user.
-  const newFiles = [
+
+  // Check if new files haven't been created yet
+  [
     "src/config/application.js",        //-> prior to 0.1.6
     "src/config/webpack-additions.js",  //-> prior to 0.1.12
     "src/config/redux-middleware.js",   //-> prior to 0.1.12
     "src/config/.Dockerfile",           //-> prior to 0.2.0
     ".dockerignore"                     //-> prior to 0.3.6
-  ];
-  newFiles.forEach((filePath) => {
-    try {
-      fs.statSync(path.join(CWD, filePath));
-    }
-    catch (e) {
-      const fileName = path.parse(filePath).base;
-      const newFile = fs.readFileSync(path.join(__dirname, "..", "..", "templates", "new", filePath), "utf8");
-      replaceFile(fileName, newFile);
-    }
-  });
+  ].forEach(testAndReplace(fs.existsSync));
 
-  // Compare contents of our hidden files, if they do not match up then auto
-  // update
+
+  // Check hidden files for changes, and overwrite if needed
   [
-    ".entry.js",
-    ".store.js",
-    ".Dockerfile"   //-> last updated in 0.2.0
-  ].forEach((fileName) => {
-    const currentFile = fs.readFileSync(getCurrentFilePath(fileName));
-    const currentSha = sha1(currentFile);
-
-    const newFile = fs.readFileSync(path.join(__dirname, "..", "..", "templates", "new", "src", "config", fileName), "utf8");
-    const newSha = sha1(newFile);
-
-    if (currentSha !== newSha) {
-      replaceFile(fileName, newFile);
-    }
-  });
+    "src/config/.entry.js",
+    "src/config/.store.js",
+    "src/config/.Dockerfile"   //-> last updated in 0.2.0
+  ].forEach(testAndReplace(hasFileChanged));
 
 
   // -> prior to 0.2.2
@@ -80,3 +95,4 @@ module.exports = async function () {
   // -> prior to 0.2.9
   await updateBabelConfig();
 };
+
