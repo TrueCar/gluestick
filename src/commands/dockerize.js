03e3645d079fe846e0b6ad9d60f2925d0921bfd7
Chris Eich <cjeich@gmail.com>
2016-05-19 15:52:14 -0700

    Add check for docker before dockerizing

--- src/commands/dockerize.js
+++ src/commands/dockerize.js
@@ -1,9 +1,15 @@
 import { spawn } from "cross-spawn";
 import path from "path";
 import process from "process";
+import commandExists from "command-exists";
 
 module.exports = function (name) {
-  // @TODO: check if docker is installed first (https://github.com/TrueCar/gluestick/issues/128)
-  spawn("docker", ["build", "-t", name, "-f", path.join(process.cwd(), "src", "config", ".Dockerfile"), process.cwd()], {stdio: "inherit"});
+  // Check if docker is installed first
+  commandExists("docker", function(err, commandExists) {
+    if(commandExists) {
+      spawn("docker", ["build", "-t", name, "-f", path.join(process.cwd(), "src", "config", ".Dockerfile"), process.cwd()], {stdio: "inherit"});
+    } else {
+      console.warn("You must install docker before continuing");
+    }
+  });
 };
-
