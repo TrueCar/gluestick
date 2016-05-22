edc704dd1bfad6c2e0244b698f5018a05903610f
Chris Eich <cjeich@gmail.com>
2016-05-21 21:48:12 -0700

    Replace commandExists module with shell js

--- src/commands/dockerize.js
+++ src/commands/dockerize.js
@@ -1,16 +1,14 @@
 import { spawn } from "cross-spawn";
 import path from "path";
 import process from "process";
-import commandExists from "command-exists";
 import logger from "../lib/logger.js";
+import { which } from "shelljs";
 
 module.exports = function (name) {
   // Check if docker is installed first
-  commandExists("docker", function(err, commandExists) {
-    if(commandExists) {
-      spawn("docker", ["build", "-t", name, "-f", path.join(process.cwd(), "src", "config", ".Dockerfile"), process.cwd()], {stdio: "inherit"});
-    } else {
-      logger.warn("You must install docker before continuing");
-    }
-  });
+  if( which("docker") !== null ) {
+    spawn("docker", ["build", "-t", name, "-f", path.join(process.cwd(), "src", "config", ".Dockerfile"), process.cwd()], {stdio: "inherit"});
+  } else {
+    logger.warn("You must install docker before continuing");
+  }
 };
