fa5a5d899d26919410f51ecd3d3c9dce8ecd4f7f
Christine Brass <cbrass@truecar.com>
2016-05-26 11:31:25 -0400

    Cleanup transform of HTML4 attributes
    Allows more flexible setting of the transformable attributes

--- src/components/BodyAttributes.js
+++ src/components/BodyAttributes.js
@@ -1,6 +1,11 @@
 import { Component, Children, PropTypes } from "react";
 import withSideEffect from "react-side-effect";
 
+
+const supportedHTML4Attributes = {
+  "bgColor": "bgcolor"
+};
+
 class BodyAttributes extends Component {
   render() {
     return Children.only(this.props.children);
@@ -13,11 +18,8 @@ BodyAttributes.propTypes = {
 
 function reducePropsToState(propsList) {
   const attrs = {};
-  const transformedAttrs = {};
   propsList.forEach(function (props) {
-    if (props.hasOwnProperty("bgColor")) {
-      Object.assign(transformedAttrs, {"data-oy-bgcolor": props["bgColor"]});
-    }
+    const transformedAttrs = transformHTML4Props(props);
     Object.assign(attrs, props, transformedAttrs);
   });
   return attrs;
@@ -27,6 +29,23 @@ function handleStateChangeOnClient(attrs) {
   for (const key in attrs) {
     document.body.setAttribute(key, attrs[key]);
   }
+}
+
+function transformHTML4Props(props) {
+  const transformedProps = {};
+
+  // Provide support for HTML4 attributes on the body tag for
+  // e-mail purposes. Convert tags to ones oy-vey can translate
+  // during the render.
+  Object.keys(supportedHTML4Attributes).forEach(propName => {
+    if (props.hasOwnProperty(propName)) {
+      const name = supportedHTML4Attributes[propName];
+      const value = props[propName];
+      const transformedProp = { [`data-oy-${name}`]: value };
+      Object.assign(transformedProps, transformedProp);
+    }
+  });
+  return transformedProps;
 }
 
 export default withSideEffect(
