a999ccba25c22bde73e2d527dbebecfb02b99d22
Todd Williams <toddsurfs@icloud.com>
2016-06-24 15:26:57 -0700

    Forward cookies to browser

--- src/lib/getHttpClient.js
+++ src/lib/getHttpClient.js
@@ -7,7 +7,7 @@ import axios from "axios";
  * https://github.com/mzabriskie/axios#request-config
  * @param {axios} [httpClient] optionally override axios (used for tests/mocking)
  */
-export default function getHttpClient (options={}, req, httpClient=axios) {
+export default function getHttpClient (options={}, req, serverResponse, httpClient=axios) {
   if (!req) {
     return httpClient.create(options);
   }
@@ -17,7 +17,7 @@ export default function getHttpClient (options={}, req, httpClient=axios) {
 
   // If a request object is provided, then we want to merge the custom headers
   // with the headers that we sent from the browser in the request.
-  return httpClient.create({
+  const client = httpClient.create({
     baseURL: protocol + req.headers.host,
     headers: {
       ...req.headers,
@@ -25,5 +25,19 @@ export default function getHttpClient (options={}, req, httpClient=axios) {
     },
     ...httpConfig
   });
+
+  client.interceptors.response.use((response) => {
+    // @TODO: This will append all of the cookies sent back from server side
+    // requests in the initial page load. There is a potential issue if you are
+    // hitting 3rd party APIs. If site A sets a cookie and site B sets a cookie
+    // with the same key, then it will overwrite A's cookie and possibly create
+    // undesired effects. Currently, the suggested solution for dealing with
+    // this problem is to make the API requests to A or B in the browser and
+    // not in gsBeforeRoute for apps where that is an issue.
+    serverResponse.append("Set-Cookie", response.headers["set-cookie"]);
+    return response;
+  });
+
+  return client;
 }
 
