6d284c995c30880708b43669653454521f35e9b8
Todd Williams <toddsurfs@icloud.com>
2016-08-29 10:57:18 -0700

    update code to work in test env

--- src/lib/server/getRenderRequirementsFromEntrypoints.js
+++ src/lib/server/getRenderRequirementsFromEntrypoints.js
@@ -5,22 +5,27 @@ import { getLogger } from "./logger";
 const logger = getLogger();
 import { getHttpClient } from "gluestick-shared";
 
-const entryPoints = getWebpackEntries();
+const cachedEntryPoints = getWebpackEntries();
 
 /**
  * Sort through all of the entry points based on the number of `/` characters
  * found in the url. It will test the most deeply nested entry points first
  * while finally falling back to the default index parameter.
  */
-const sortedEntries = Object.keys(entryPoints).sort((a, b) => {
-  const bSplitLength = b.split("/").length;
-  const aSplitLength = a.split("/").length;
-  if (bSplitLength === aSplitLength) {
-    return b.length - a.length;
-  }
+const getSortedEntries = function () {
+  return Object.keys(getWebpackEntries()).sort((a, b) => {
+    const bSplitLength = b.split("/").length;
+    const aSplitLength = a.split("/").length;
+    if (bSplitLength === aSplitLength) {
+      return b.length - a.length;
+    }
 
-  return bSplitLength - aSplitLength;
-});
+    return bSplitLength - aSplitLength;
+  });
+};
+
+// Cache this result so it only runs once in production
+const cachedSortedEntries = getSortedEntries();
 
 /**
  * This method takes the server request object, determines which entry point
@@ -31,6 +36,15 @@ const sortedEntries = Object.keys(entryPoints).sort((a, b) => {
 export default function getRenderRequirementsFromEntrypoints (req, res, config={}, customRequire=require) {
   const httpClient = getHttpClient(config.httpClient, req, res);
   const { path: urlPath } = parseURL(req.url);
+  let sortedEntries, entryPoints;
+  if (process.env.NODE_ENV === "production") {
+    sortedEntries = cachedSortedEntries;
+    entryPoints = cachedEntryPoints;
+  }
+  else {
+    sortedEntries = getSortedEntries();
+    entryPoints = getWebpackEntries();
+  }
 
   logger.debug("Getting webpack entries");
 
