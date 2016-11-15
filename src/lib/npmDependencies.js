6220638c95a3305ba59ed5ccc5a517dd257d4da2
Christine Brass <cbrass@truecar.com>
2016-11-15 18:36:00 -0500

    Dedupe install code

--- src/lib/npmDependencies.js
+++ src/lib/npmDependencies.js
@@ -1,12 +1,21 @@
+import path from "path";
+import rimraf from "rimraf";
 const spawn = require("cross-spawn").spawn;
 
 const IS_WINDOWS = process.platform === "win32";
 
 function install () {
   const postFix = IS_WINDOWS ? ".cmd" : "";
-  spawn("npm" + postFix, ["install"], {stdio: "inherit"});
+  return spawn("npm" + postFix, ["install"], {stdio: "inherit"});
+}
+
+function cleanSync () {
+  // wipe the existing node_modules folder so we can have a clean start
+  rimraf.sync(path.join(process.cwd(), "node_modules"));
+  spawn.sync("npm", ["cache", "clean"], {stdio: "inherit"});
 }
 
 module.exports = {
+  cleanSync: cleanSync,
   install: install
 };
