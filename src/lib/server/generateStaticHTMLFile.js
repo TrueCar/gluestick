51361174c78f525193bb37e3e83312d0d86d58a6
Marc Delagrammatikas <delagrammatikas@gmail.com>
2016-11-23 19:08:06 -0800

    Write the response output to index.html in the build folder.

--- src/lib/server/generateStaticHTMLFile.js
+++ src/lib/server/generateStaticHTMLFile.js
@@ -1,6 +1,7 @@
 import path from "path";
 import { prepareOutput } from "./RequestHandler";
 import { getLogger } from "./logger";
+import fs from "fs-extra";
 import WebpackIsomorphicTools from "webpack-isomorphic-tools";
 import webpackConfig from "../../config/webpack-isomorphic-tools-config";
 import detectEnvironmentVariables from "../detectEnvironmentVariables";
@@ -23,11 +24,10 @@ export default async function () {
         const envVars = detectEnvironmentVariables(path.join(process.cwd(), "src", "config", "application.js"));
         const Index = require(path.join(process.cwd(), "Index.js")).default;
         const output = await prepareOutput(mockReq, {Index}, {}, {}, envVars, true);
-        LOGGER.debug(output.responseString);
+        fs.writeFileSync(path.join(process.cwd(), "build", "index.html"), output.responseString);
       }
       catch (e) {
         LOGGER.error(e);
       }
     });
 }
-
