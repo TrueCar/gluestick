abcba305ddf6b449a14c0b3efa03a78fd3bfa952
Paweł Trysła <pawel.trysla@gmail.com>
2017-01-23 16:57:00 +0100

    #419: (example-like) predefined component generator

---
+++ src/generator/predefined/component.js
@@ -0,0 +1,67 @@
+const createTemplate = require('../createTemplate');
+
+const classComponentTemplate = createTemplate`
+/* @flow */
+import React, { Component } from "react";
+
+export default class ${args => args.name} extends Component {
+  render () {
+    return (
+      <div>
+        ${args => args.name}
+      </div>
+    );
+  }
+}
+`;
+
+const functionalComponentTemplate = createTemplate`
+/* @flow */
+import React from "react";
+
+export default function ${args => args.name} () {
+  return (
+    <div>
+       ${args => args.name}
+    </div>
+  );
+}
+`;
+
+const testTemplate = createTemplate`
+/*global React*/
+/*global describe it*/
+/*global expect*/
+import ${args => args.name} from "${args => args.path}";
+import { shallow } from "enzyme";
+
+describe("${args => args.path}", () => {
+  it("renders without an issue", () => {
+    const subject = <${args => args.name} />;
+    const wrapper = shallow(subject);
+    expect(wrapper).to.exist;
+  });
+});
+`;
+
+module.exports = exports = {
+  name: "component",
+  args: options => ({
+    name: options.name
+  }),
+  entries: [
+    options => ({
+      path: "src/components",
+      filename: options.name,
+      template: options.functional ? functionalComponentTemplate : classComponentTemplate,
+    }),
+    options => ({
+      path: "test/components",
+      filename: `${options.name}.test`,
+      template: testTemplate,
+      args: {
+        path: `../src/components/${options.name}`
+      }
+    })
+  ]
+};
