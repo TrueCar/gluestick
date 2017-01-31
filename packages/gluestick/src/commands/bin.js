3348e0ae45bce432f54217c62ed90825f0166453
lukewalczak <lukasz.walczak@callstack.io>
2017-01-31 12:49:00 +0100

    Inital reafactor

--- packages/gluestick/src/commands/bin.js
+++ packages/gluestick/src/commands/bin.js
@@ -1,15 +1,15 @@
-import { spawn } from "cross-spawn";
-import path from "path";
+const spawn = require("cross-spawn");
+const path = require("path");
 
 // Creates patch to dependency's bin directory
 const getDependencyPath = (name) =>
   path.join(__dirname, "..", "..", "node_modules", ".bin", name);
 
 // `opts` is array of options with Command object attached as last element
-module.exports = function(dependencyName, ...opts) {
+module.exports = function(context, dependencyName, ...opts) {
   spawn(
     getDependencyPath(dependencyName),
-    opts.splice(0, opts.length - 1),
+    opts[opts.length - 1].parent.rawArgs.slice(4),
     {
       stdio: "inherit",
     }
