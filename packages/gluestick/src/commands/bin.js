5ea74911f5ba8c909cd578f00dd81cbac6fa6866
lukewalczak <lukasz.walczak@callstack.io>
2017-02-01 14:39:51 +0100

    Fixed eslint

--- packages/gluestick/src/commands/bin.js
+++ packages/gluestick/src/commands/bin.js
@@ -1,15 +1,15 @@
 /* @flow */
+import type { Context } from '../types.js';
 
-const spawn = require("cross-spawn");
-const path = require("path");
-import type { Context } from  "../types.js";
+const spawn = require('cross-spawn');
+const path = require('path');
 
 // Creates patch to dependency's bin directory
 const getDependencyPath = name =>
   path.join(__dirname, '..', '..', 'node_modules', '.bin', name);
 
 // `opts` is array of options with Command object attached as last element
-module.exports = function(context: Context, dependencyName: string, ...opts: Array<*>) {
+module.exports = (context: Context, dependencyName: string, ...opts: Array<*>) => {
   spawn(
     getDependencyPath(dependencyName),
     opts[opts.length - 1].parent.rawArgs.slice(4),
--- packages/gluestick/test/commands/bin.test.js
+++ packages/gluestick/test/commands/bin.test.js
@@ -1,8 +1,9 @@
 const spawnMock = jest.fn();
 jest.setMock('cross-spawn', spawnMock);
 
-const path =  require('path');
+const path = require('path');
 const bin = require('../../src/commands/bin');
+
 const spawnOptions = { stdio: 'inherit' };
 
 const getDependencyPath = name =>
@@ -11,8 +12,8 @@ const getDependencyPath = name =>
 describe('cli: gluestick bin', () => {
   it('runs the dependency without any options', () => {
     const dependencyName = 'fakeDep';
-    
-    bin({}, dependencyName, { parent: { rawArgs: [] }});
+
+    bin({}, dependencyName, { parent: { rawArgs: [] } });
     expect(spawnMock).toBeCalledWith(getDependencyPath(dependencyName), [], spawnOptions);
   });
 
@@ -20,7 +21,7 @@ describe('cli: gluestick bin', () => {
     const dependencyName = 'fakeDep';
     const dependencyOptions = ['test', '-T', '--test'];
     const commanderObject = {
-      parent: { rawArgs: ['', '', '', '', ...dependencyOptions] }
+      parent: { rawArgs: ['', '', '', '', ...dependencyOptions] },
     };
 
     bin({}, dependencyName, ...dependencyOptions, commanderObject);
