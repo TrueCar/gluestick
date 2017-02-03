0165a6eb86ed2f5c7f66cd879072c743f81bbe0d
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-02-03 13:21:57 -0800

    Pass default config values when building with -Z and --static.
    Fixes undefined config.server.loadjs in getHead.js:37:48

--- src/lib/server/generateStaticHTMLFile.js
+++ src/lib/server/generateStaticHTMLFile.js
@@ -6,8 +6,23 @@ import WebpackIsomorphicTools from "webpack-isomorphic-tools";
 import webpackConfig from "../../config/webpack-isomorphic-tools-config";
 import detectEnvironmentVariables from "../detectEnvironmentVariables";
 import getRenderRequirementsFromEntrypoints from "./getRenderRequirementsFromEntrypoints";
+import loadServerConfig from "./loadServerConfig";
 
 const LOGGER = getLogger();
+
+let config = {};
+if (process.env.NODE_ENV !== "test") {
+  const GLOBAL_CONFIG_PATH = path.join(process.cwd(), "src", "config", "application");
+  const SERVER_CONFIG = loadServerConfig();
+  const GLOBAL_CONFIG = require(GLOBAL_CONFIG_PATH).default;
+
+  config = {
+    ...GLOBAL_CONFIG,
+    server: {
+      ...SERVER_CONFIG
+    }
+  };
+}
 
 export default async function () {
   const mockReq = {
@@ -27,7 +42,7 @@ export default async function () {
       try {
         const envVars = detectEnvironmentVariables(path.join(process.cwd(), "src", "config", "application.js"));
         const renderRequirements = getRenderRequirementsFromEntrypoints(mockReq, mockRes, mockConfig);
-        const output = await prepareOutput(mockReq, renderRequirements, {}, {}, envVars, true);
+        const output = await prepareOutput(mockReq, renderRequirements, {}, config, envVars, true);
         fs.writeFileSync(path.join(process.cwd(), "build", "index.html"), output.responseString);
       }
       catch (e) {
