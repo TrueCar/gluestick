4f56b0f6311e5b5bfebad3dc475bda55c7f7353e
lukewalczak <lukasz.walczak@callstack.io>
2017-02-06 11:46:21 +0100

    Added flowtype

--- packages/gluestick/src/generator/createTemplate.js
+++ packages/gluestick/src/generator/createTemplate.js
@@ -1,15 +1,24 @@
 /* @flow */
+type Interpolation = any;
+type TemplateCreator = (args: Object) => string;
+type TemplateCreatorFactory = (
+  strings: string[],
+  interpolations: Interpolation[]
+) => TemplateCreator;
 
-const tag = (spacesToRemove, strings, ...interpolations) => {
+const tag = (
+  spacesToRemove: number,
+  strings: string[],
+  ...interpolations: Interpolation[]
+): TemplateCreator => {
   /**
    * Creates template populated with given arguments.
-   *
-   * @param {any} args Arguments to pass to template
-   * @returns {String}
+   * @param {object} args Arguments to pass to template
+   * @returns {string}
    */
 
-  return (args: Object) => {
-    const interpolate = (interpolation) => {
+  return (args: Object): string => {
+    const interpolate = (interpolation: Interpolation): string => {
       if (typeof interpolation === 'function') {
         if (!args) {
           throw new Error('No arguments for for template were passed');
@@ -20,16 +29,17 @@ const tag = (spacesToRemove, strings, ...interpolations) => {
       }
       return '';
     };
-    const entries = interpolations.reduce((previous, current, index) => {
-      const interpolatedCurrentValue = interpolate(current);
-      return previous.concat(
-      index < strings.length - 1
-        ? [interpolatedCurrentValue, strings[index + 1]]
-        : [interpolatedCurrentValue],
-      );
-    }, [strings[0]]);
+    const entries: string[] =
+      interpolations.reduce((previous: string[], current: any, index: number) => {
+        const interpolatedCurrentValue: string = interpolate(current);
+        return previous.concat(
+        index < strings.length - 1
+          ? [interpolatedCurrentValue, strings[index + 1]]
+          : [interpolatedCurrentValue],
+        );
+      }, [strings[0]]);
 
-    const finalString = entries.join('').split('\n').map((line) => {
+    const finalString: string = entries.join('').split('\n').map((line: string): string => {
       if (new RegExp(`[^\\S\\n]{${spacesToRemove}}`, 'i').test(line)) {
         return line.substring(spacesToRemove);
       }
@@ -42,15 +52,16 @@ const tag = (spacesToRemove, strings, ...interpolations) => {
 
 
 /**
- * Creates factory function that returs string from given template.
+ * Creates factory function that returns string from given template.
  *
- * @param {Array<String>} strings Strings from template
+ * @param {Array<any>} strings Strings from template
  * @param {Array<any>} interpolations Interpolations from template
- * @returns {Function}
+ * @returns {TemplateCreator} returns string
  */
-module.exports = exports = (...params: Array<any>) => {
+
+module.exports = exports = (...params: Array<any>): TemplateCreator | TemplateCreatorFactory => {
   if (typeof params[0] === 'number') {
-    return (strings: string[], ...interpolations: Array<any>) =>
+    return (strings: string[], ...interpolations: Array<any>): TemplateCreator =>
       tag(params[0], strings, ...interpolations);
   } else if (Array.isArray(params[0])) {
     return tag(0, ...params);
