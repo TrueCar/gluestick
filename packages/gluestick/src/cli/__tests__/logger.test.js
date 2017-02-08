43667cfb32c03cc7e33434888cbf73f80163e5c2
ferrannp <fnp.developer@gmail.com>
2017-02-08 12:25:34 +0100

    Added custom matcher for better error message in logger.

--- packages/gluestick/src/cli/__tests__/logger.test.js
+++ packages/gluestick/src/cli/__tests__/logger.test.js
@@ -2,9 +2,16 @@ const logger = require('../logger');
 const colorScheme = require('../colorScheme');
 
 describe('logger', () => {
+  expect.extend({
+    toContainKey(received, item) {
+      const pass = !!received[item] === true;
+      return { actual: received, message: () => `Key '${item}' not found in ${received}`, pass };
+    },
+  });
+
   it('checks that logger options match with colorScheme', () => {
     Object.keys(logger).forEach((key) => {
-      expect(colorScheme[key]).toBeDefined();
+      expect(colorScheme).toContainKey(key);
     });
   });
 });
