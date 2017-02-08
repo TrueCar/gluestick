02f717a2573dd8514aa7cff8fe3006905387664e
ferrannp <fnp.developer@gmail.com>
2017-02-08 14:28:27 +0100

    #503: Added flow-typed for Jest and flow to logger.test.js.

--- packages/gluestick/src/cli/__tests__/logger.test.js
+++ packages/gluestick/src/cli/__tests__/logger.test.js
@@ -1,17 +1,25 @@
+/* @flow */
+
 const logger = require('../logger');
 const colorScheme = require('../colorScheme');
 
 describe('logger', () => {
-  expect.extend({
-    toContainKey(received, item) {
-      const pass = !!received[item] === true;
-      return { actual: received, message: () => `Key '${item}' not found in ${received}`, pass };
-    },
+  // $FlowFixMe Flow doesn't like that we assign console.log to a mock function
+  console.log = jest.fn();
+
+  const message = 'Some test message';
+
+  Object.keys(logger).forEach(key => {
+    if (key !== 'error') {
+      it(`logs ${key}`, () => {
+        logger[key](message);
+        expect(console.log).toBeCalledWith('[GlueStick]', colorScheme[key](message));
+      });
+    }
   });
 
-  it('checks that logger options match with colorScheme', () => {
-    Object.keys(logger).forEach((key) => {
-      expect(colorScheme).toContainKey(key);
-    });
+  it('logs error', () => {
+    logger.error('Errors are bad');
+    expect(console.log).toBeCalledWith('[GlueStick]', 'ERROR: ', colorScheme.error('Errors are bad'));
   });
 });
--- packages/gluestick/src/cli/colorScheme.js
+++ packages/gluestick/src/cli/colorScheme.js
@@ -1,4 +1,8 @@
-import chalk from 'chalk';
+/* @flow */
+
+import type { LoggerTypes } from '../types';
+
+const chalk = require('chalk');
 
 const success = chalk.green;
 const info = chalk.yellow;
@@ -8,7 +12,12 @@ const highlight = chalk.bold;
 const debug = chalk.blue;
 const error = chalk.red;
 
-module.exports = {
+type ColorScheme = LoggerTypes & {
+  filename: Function;
+  highlight: Function;
+}
+
+const colorScheme: ColorScheme = {
   success,
   info,
   warn,
@@ -17,3 +26,5 @@ module.exports = {
   filename,
   highlight,
 };
+
+module.exports = colorScheme;
