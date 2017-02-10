b0518eb51dd58781cdf4a3fcccd286e5c0972455
Paweł Trysła <pawel.trysla@gmail.com>
2017-02-10 11:41:46 +0100

    preparePlugins test

---
+++ packages/gluestick/src/config/__tests__/preparePlugins.test.js
@@ -0,0 +1,27 @@
+//jest.mock('testPlugin');
+const path = require('path');
+const preparePlugins = require('../preparePlugins');
+
+const originalPath = path.join.bind({});
+
+describe('config/preparePlugins', () => {
+  beforeEach(() => {
+    path.join = jest.fn().mockImplementation(() => './__tests__/testPlugin.js');
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+    path.join = originalPath;
+  });
+
+  it('should return empty array if plugins field is not an array', () => {
+    expect(preparePlugins({})).toEqual([]);
+  });
+
+  it('should return plugins array', () => {
+    expect(preparePlugins({ plugins: ['testPlugin'] })).toEqual([{
+      name: 'testPlugin',
+      body: 'body of testPlugin',
+    }]);
+  });
+});
