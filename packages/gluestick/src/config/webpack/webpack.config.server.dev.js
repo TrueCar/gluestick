c0a287aa1a59e0fd866faab8a1da0c8d2d365c57
dratwas <drapich.piotr@gmail.com>
2017-02-10 14:18:22 +0100

    add compilation timestamp to process.env

--- packages/gluestick/src/config/webpack/webpack.config.server.dev.js
+++ packages/gluestick/src/config/webpack/webpack.config.server.dev.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import type { WebpackConfig } from '../../types';
+const webpack = require('webpack');
 
 const deepClone = (obj: Object) => {
   const output: Object = {};
@@ -17,6 +18,13 @@ const deepClone = (obj: Object) => {
 
 module.exports = (serverConfig: WebpackConfig, devServerPort: number): WebpackConfig => {
   const configuration: Object = deepClone(serverConfig);
+  configuration.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        COMPILATION_TIMESTAMP: JSON.stringify(new Date().toISOString()),
+      },
+    }),
+  );
   configuration.output.publicPath = `http://localhost:${devServerPort}${
     configuration.output.publicPath}`;
   return configuration;
