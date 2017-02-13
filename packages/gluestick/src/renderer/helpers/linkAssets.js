b74ff66c3507872089398f144e88f004307ee6c8
dratwas <drapich.piotr@gmail.com>
2017-02-13 16:52:00 +0100

    fix issue with undefined assets

--- packages/gluestick/src/renderer/helpers/linkAssets.js
+++ packages/gluestick/src/renderer/helpers/linkAssets.js
@@ -6,7 +6,6 @@ const isProduction = process.env.NODE_ENV === 'production';
 const getAssetPathForFile = (filename, section, webpackAssets) => {
   const assets = webpackAssets[section] || {};
   const webpackPath = assets[filename];
-
   return webpackPath;
 };
 
@@ -22,7 +21,7 @@ module.exports = (config, entryPoint, assets) => {
       <link key={key++} rel="stylesheet" type="text/css" href={stylesHref} />,
     );
     // Need to explicitly check if there is CSS vendor bundle and include it
-    const vendorStylesHref = getAssetPathForFile('vendor', 'styles');
+    const vendorStylesHref = getAssetPathForFile('vendor', 'styles', assets);
     if (vendorStylesHref) {
       styleTags.push(
         <link key={key++} rel="stylesheet" type="text/css" href={vendorStylesHref} />,
