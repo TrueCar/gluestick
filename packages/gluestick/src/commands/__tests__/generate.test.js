7073fd06ff1e9b84b18deec5e9f0098e5a138a5a
ferrannp <fnp.developer@gmail.com>
2017-02-14 12:07:00 +0100

    Fix build errors on generate.test.js.

--- packages/gluestick/src/commands/__tests__/generate.test.js
+++ packages/gluestick/src/commands/__tests__/generate.test.js
@@ -7,8 +7,8 @@ const generator = require('../../generator');
 const logger = require('../../cli/logger');
 
 describe('cli: gluestick generate', () => {
-
   // Needed for test not to finish when calling generate
+  // $FlowFixMe Flow doesn't like that we assign process.exit to a mock function
   process.exit = jest.fn();
 
   it('calls the generator with correct parameters', () => {
