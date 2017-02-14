2d36dbd43c4eeab2e2590a473e28ab5dec4ad58d
ferrannp <fnp.developer@gmail.com>
2017-02-14 11:23:11 +0100

    Fix generate.test.js.

--- packages/gluestick/src/commands/__tests__/generate.test.js
+++ packages/gluestick/src/commands/__tests__/generate.test.js
@@ -7,6 +7,10 @@ const generator = require('../../generator');
 const logger = require('../../cli/logger');
 
 describe('cli: gluestick generate', () => {
+
+  // Needed for test not to finish when calling generate
+  process.exit = jest.fn();
+
   it('calls the generator with correct parameters', () => {
     const config = { plugins: [] };
     generate({ config, logger }, 'component', 'MyComponent', {});
