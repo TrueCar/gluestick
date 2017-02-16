830e90bb2764832fe4380135e4531ca34a348dfb
Michal Chudziak <michal.chudziak@gmail.com>
2017-02-16 14:20:51 +0100

    Remove unused dependencies form generated project.

--- packages/gluestick/src/generator/templates/package.js
+++ packages/gluestick/src/generator/templates/package.js
@@ -18,7 +18,19 @@ module.exports = (createTemplate: CreateTemplate) => createTemplate`
     "gluestick": "${
       (args) => args.dev || '1.0.0' // TODO: replace with real version
     }",
-    "axios": "0.12.0",
+    "gluestick-shared": "0.4.22",
+    "react": "15.4.2",
+    "react-dom": "15.4.2",
+    "react-helmet": "4.0.0",
+    "react-redux": "5.0.2",
+    "react-router": "3.0.2",
+    "redux": "3.6.0"
+  },
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "babel-eslint": "7.1.1",
+    "babel-jest": "18.0.0",
     "babel-core": "6.22.1",
     "babel-loader": "6.2.10",
     "babel-plugin-gluestick": "0.0.1",
@@ -29,31 +41,6 @@ module.exports = (createTemplate: CreateTemplate) => createTemplate`
     "babel-register": "6.22.0",
     "babel-runtime": "6.22.0",
     "babel-traverse": "6.22.1",
-    "css-loader": "0.26.1",
-    "electrode-react-ssr-caching": "0.1.5",
-    "file-loader": "0.9.0",
-    "gluestick-shared": "0.4.22",
-    "history": "3.0.0",
-    "image-webpack-loader": "3.1.0",
-    "json-loader": "0.5.4",
-    "node-sass": "4.3.0",
-    "oy-vey": "0.8.0",
-    "radium": "0.18.1",
-    "react": "15.4.2",
-    "react-dom": "15.4.2",
-    "react-helmet": "4.0.0",
-    "react-redux": "5.0.2",
-    "react-router": "3.0.2",
-    "redux": "3.6.0",
-    "sass-loader": "4.1.1",
-    "style-loader": "0.13.1",
-    "webpack": "2.2.0"
-  },
-  "author": "",
-  "license": "ISC",
-  "devDependencies": {
-    "babel-eslint": "7.1.1",
-    "babel-jest": "18.0.0",
     "babel-plugin-react-transform": "2.0.2",
     "enzyme": "2.7.1",
     "eslint": "3.14.1",
@@ -68,4 +55,3 @@ module.exports = (createTemplate: CreateTemplate) => createTemplate`
   }
 }
 `;
-
