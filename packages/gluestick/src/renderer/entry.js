a29d95a8e14d42e8a3815e176ca8dfbcfaaa64f4
Paweł Trysła <pawel.trysla@gmail.com>
2017-02-16 14:06:14 +0100

    add entry test

--- packages/gluestick/src/renderer/entry.js
+++ packages/gluestick/src/renderer/entry.js
@@ -1,11 +1,19 @@
+import type { Logger, GSConfig } from '../types';
+
+const util = require('util');
+
 // Read config from command line arguments.
-const config = JSON.parse(process.argv[2]);
+const config: GSConfig = JSON.parse(process.argv[2] || '{}');
 
 // Prepare logger.
-const loggerFactory = type => {
+const loggerFactory = (type: string): (values: Array<*>) => void => {
   if (process.env.NODE_ENV === 'production') {
     return (...values) => {
-      const log = `${type.toUpperCase()}: ${values.reduce((p, c) => p.concat(c), '')}`;
+      const log = `${type.toUpperCase()}: ${
+        values.reduce((prev, curr) => {
+          return prev.concat(typeof curr === 'string' ? curr : util.inspect(curr, { depth: 4 }));
+        }, '')
+      }`;
       if (type === 'error') {
         process.stderr.write(log);
       } else {
@@ -13,11 +21,11 @@ const loggerFactory = type => {
       }
     };
   }
-  return (...value) => {
-    process.send({ type, value });
+  return (...values) => {
+    process.send({ type, value: values });
   };
 };
-const logger = {
+const logger: Logger = {
   info: loggerFactory('info'),
   success: loggerFactory('success'),
   error: loggerFactory('error'),
