c31a7f8f704d37e15c2556c0a276edea65c17f17
dratwas <drapich.piotr@gmail.com>
2017-02-17 14:40:10 +0100

    full refactor error handler and tests for it

--- packages/gluestick/src/renderer/helpers/__tests__/errorHandler.test.js
+++ packages/gluestick/src/renderer/helpers/__tests__/errorHandler.test.js
@@ -6,11 +6,8 @@ jest.mock('handlebars', () => ({
 }));
 
 const errorHandler = require('../errorHandler');
-const path = require('path');
 const fs = require('fs');
 
-const originalPathJoin = path.join.bind(path);
-const originalFsStat = fs.stat.bind(fs);
 const originalFsReadFile = fs.stat.bind(fs);
 
 const response = {
@@ -21,9 +18,6 @@ const response = {
 
 describe('renderer/helpers/errorHandler', () => {
   beforeEach(() => {
-    path.join = jest.fn((...values) => {
-      return values.indexOf('gluestick') ? 'default-505.hbs' : 'custom-505.hbs';
-    });
     response.status = jest.fn();
     response.send = jest.fn();
     response.sendStatus = jest.fn();
@@ -31,60 +25,55 @@ describe('renderer/helpers/errorHandler', () => {
 
   afterAll(() => {
     jest.resetAllMocks();
-    path.join = originalPathJoin;
-    fs.stat = originalFsStat;
     fs.readFile = originalFsReadFile;
   });
 
-  it('should render custom error page', () => {
-    fs.stat = jest.fn((filename, callback) => {
-      callback(null, { isFile: () => true });
-    });
+  it('should render custom error page', async () => {
     fs.readFile = jest.fn((filename, options, callback) => {
       callback(null, 'custom');
     });
-    errorHandler({}, response, {});
+    await errorHandler({}, response, {});
     expect(response.send.mock.calls[0][0]).toEqual('custom');
   });
 
-  xit('should return 500 if error while reading custom template occured', () => {
-    fs.stat = jest.fn((filename, callback) => {
-      callback(null, { isFile: () => true });
-    });
+  it('should return 500 if error while reading custom template occured', async () => {
     fs.readFile = jest.fn((filename, options, callback) => {
       callback('error', null);
     });
-    errorHandler({}, response, {});
+    await errorHandler({}, response, {});
     expect(response.sendStatus.mock.calls[0][0]).toEqual(500);
   });
 
-  it('should render default error page', () => {
-    fs.stat = jest.fn((filename, callback) => {
-      callback(null, { isFile: () => true });
-    });
+  it('should render default error page', async () => {
     fs.readFile = jest.fn((filename, options, callback) => {
-      callback(null, 'default');
+      if (filename === 'custom-505.hbs') {
+        callback({ code: 'ENOENT' }, null);
+      } else if (filename === 'default-505.hbs') {
+        callback(null, 'default');
+      }
+      callback('error', null);
     });
-    errorHandler({}, response, {});
+    await errorHandler({}, response, {}, 'custom-505.hbs', 'default-505.hbs');
     expect(response.send.mock.calls[0][0]).toEqual('default');
   });
 
-  xit('should return 500 if error while reading default template occured', () => {
-    fs.stat = jest.fn((filename, callback) => {
-      callback(null, { isFile: () => filename === 'default-505.hbs' });
-    });
+  it('should return 500 if error while reading default template occured', async () => {
     fs.readFile = jest.fn((filename, options, callback) => {
-      callback('error', null);
+      if (filename === 'custom-505.hbs') {
+        callback({ code: 'ENOENT' }, null);
+      } else if (filename === 'default-505.hbs') {
+        callback('error', null);
+      }
     });
-    errorHandler({}, response, {});
+    await errorHandler({}, response, {}, 'custom-505.hbs', 'default-505.hbs');
     expect(response.sendStatus.mock.calls[0][0]).toEqual(500);
   });
 
-  it('should return 501 if no error page was provided', () => {
-    fs.stat = jest.fn((filename, callback) => {
-      callback(null, { isFile: () => false });
+  it('should return 501 if no error page was provided', async () => {
+    fs.readFile = jest.fn((filename, options, callback) => {
+      callback({ code: 'ENOENT' }, null);
     });
-    errorHandler({}, response, {});
+    await errorHandler({}, response, {}, 'custom-505.hbs', 'default-505.hbs');
     expect(response.sendStatus.mock.calls[0][0]).toBe(501);
   });
 });
