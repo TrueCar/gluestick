93944532fc0cbfc43a836dbe85de11e7b6d40bd1
Paweł Trysła <pawel.trysla@gmail.com>
2017-02-21 15:38:13 +0100

    #595: fully-featured autoUpgrade

--- packages/gluestick/src/autoUpgrade/index.js
+++ packages/gluestick/src/autoUpgrade/index.js
@@ -1,56 +1,61 @@
 /* @flow */
 import type { Context, UpdateDepsPromptResults, ProjectPackage } from '../types';
 
-// const fs = require('fs');
+const fs = require('fs');
 const path = require('path');
-// const sha1 = require('sha1');
+const sha1 = require('sha1');
 
 const checkForMismatch = require('./checkForMismatch');
 const updateDependencies = require('./updateDependencies');
-// const generateFile = require('./generateFile');
-// const getSingleEntryFromGenerator = require('./getSingleEntryFromGenerator');
-// const parseConfig = require('../generator/parseConfig');
-// const replaceFile = require('./replaceFile');
+const getSingleEntryFromGenerator = require('./getSingleEntryFromGenerator');
+const parseConfig = require('../generator/parseConfig');
 
-module.exports = ({ config, logger }: Context) => {
+module.exports = ({ logger }: Context) => {
   const projectPackage: ProjectPackage = require(path.join(process.cwd(), 'package.json'));
-  return checkForMismatch(projectPackage).then((results: UpdateDepsPromptResults): void => {
-    if (results.shouldFix) {
-      updateDependencies(logger, projectPackage, results.mismatchedModules);
+
+  // Compare contents of our hidden files, if they do not match up then auto update them
+  [
+    'src/config/.Dockerfile',   // -> last updated in 0.2.0
+  ].forEach((filePath: string): void => {
+    const currentHash: string = sha1(fs.readFileSync(path.join(process.cwd(), filePath)));
+    const generatorEntry: Object = getSingleEntryFromGenerator('new', path.basename(filePath), {});
+    const entryConfig = parseConfig({
+      entry: generatorEntry,
+    }, {});
+    const templateHash: string = sha1(entryConfig.entry.template);
+    if (currentHash !== templateHash) {
+      const absolutePath: string = path.join(process.cwd(), filePath);
+      logger.success(`${filePath} file is out of date. Updating at path ${absolutePath}...`);
+      fs.writeFileSync(absolutePath, entryConfig.entry.template, 'utf-8');
     }
   });
 
   // Check for certain files that we've added to new Gluestick applications.
   // If those files don't exist, add them for the user.
-  // const newFiles: string[] = [
-  //   'src/config/application.js',        // -> prior to 0.1.6
-  //   'src/config/webpack-additions.js',  // -> prior to 0.1.12
-  //   'src/config/redux-middleware.js',   // -> prior to 0.1.12
-  //   'src/config/.Dockerfile',           // -> prior to 0.2.0
-  //   '.dockerignore',                    // -> prior to 0.3.6
-  //   'src/config/init.browser.js',        // -> prior to 0.9.26
-  //   // TODO: add files from V2
-  // ];
+  [
+    'src/config/application.js',        // -> prior to 0.1.6
+    'src/config/webpack-additions.js',  // -> prior to 0.1.12
+    'src/config/redux-middleware.js',   // -> prior to 0.1.12
+    'src/config/.Dockerfile',           // -> prior to 0.2.0
+    '.dockerignore',                    // -> prior to 0.3.6
+    'src/config/init.browser.js',        // -> prior to 0.9.26
+    // TODO: add files from V2
+  ].forEach((filePath: string): void => {
+    if (!fs.existsSync(path.join(process.cwd(), filePath))) {
+      const absolutePath: string = path.join(process.cwd(), filePath);
+      logger.success(`File ${filePath} does not exist. Creating at path ${absolutePath}...`);
+      const generatorEntry: Object = getSingleEntryFromGenerator('new', path.basename(filePath), {});
+      const entryConfig = parseConfig({
+        entry: generatorEntry,
+      }, {});
+      fs.writeFileSync(absolutePath, entryConfig.entry.template, 'utf-8');
+    }
+  });
 
-  // newFiles.forEach((filePath: string): void => {
-  //   if (!fs.existsSync(path.join(process.cwd(), filePath))) {
-  //     generateFile(path.basename(filePath));
-  //   }
-  // });
-
-  // Compare contents of our hidden files, if they do not match up then auto
-  // update
-  // [
-  //   '.Dockerfile',   // -> last updated in 0.2.0
-  // ].forEach((filePath: string): void => {
-  //   const currentHash: string = sha1(fs.readFileSync(path.join(process.cwd(), filePath)));
-  //   const generatorEntry: Object = getSingleEntryFromGenerator('new', path.basename(filePath));
-  //   const entryConfig = parseConfig({
-  //     entry: generatorEntry,
-  //   }, {});
-  //   const templateHash: string = sha1(entryConfig.entry.template);
-  //   if (currentHash !== templateHash) {
-  //     replaceFile(logger, filePath, entryConfig.entry.template);
-  //   }
-  // });
+  // Update dependencies
+  return checkForMismatch(projectPackage).then((results: UpdateDepsPromptResults): void => {
+    if (results.shouldFix) {
+      updateDependencies(logger, projectPackage, results.mismatchedModules);
+    }
+  });
 };
