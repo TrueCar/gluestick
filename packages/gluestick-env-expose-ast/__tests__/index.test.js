73bfe450a9aff73451484e43a4a444baedb0c9a3
Paweł Trysła <pawel.trysla@gmail.com>
2017-02-22 16:51:54 +0100

    plugin implementation

---
+++ packages/gluestick-env-expose-ast/__tests__/index.test.js
@@ -0,0 +1,62 @@
+jest.mock('../detectEnvironmentVariables.js', () => jest.fn(
+  (file) => file.includes('0') ? ['ENV_1', 'ENV_2'] : ['ENV_3'],
+));
+jest.mock('webpack', () => ({
+  DefinePlugin: class {
+    constructor(spec) {
+      this.spec = spec;
+    }
+  }
+}));
+const expose = require('../index');
+
+test('plugin should not expose anything', () => {
+  const results = expose({}, {});
+  const clientPlugins = results.overwriteClientWebpackConfig({ plugins: [] }).plugins;
+  const serverPlugins = results.overwriteServerWebpackConfig({ plugins: [] }).plugins;
+  expect(clientPlugins.length).toBe(0);
+  expect(serverPlugins.length).toBe(0);
+});
+
+test('plugin should expose from single file', () => {
+  process.env.ENV_1 = true;
+  process.env.ENV_2 = false;
+  const results = expose({
+    parse: 'file0',
+  }, {});
+  const clientPlugins = results.overwriteClientWebpackConfig({ plugins: [] }).plugins;
+  expect(clientPlugins.length).toBe(1);
+  expect(clientPlugins[0].spec).toEqual({
+    'process.env.ENV_1': '\"true\"',
+    'process.env.ENV_2': '\"false\"',
+  });
+  const serverPlugins = results.overwriteServerWebpackConfig({ plugins: [] }).plugins;
+  expect(serverPlugins.length).toBe(1);
+  expect(serverPlugins[0].spec).toEqual({
+    'process.env.ENV_1': '\"true\"',
+    'process.env.ENV_2': '\"false\"',
+  });
+});
+
+test('plugin should expose from multiple files', () => {
+  process.env.ENV_1 = true;
+  process.env.ENV_2 = false;
+  process.env.ENV_3 = 'test';
+  const results = expose({
+    parse: ['file0', 'file1'],
+  }, {});
+  const clientPlugins = results.overwriteClientWebpackConfig({ plugins: [] }).plugins;
+  expect(clientPlugins.length).toBe(1);
+  expect(clientPlugins[0].spec).toEqual({
+    'process.env.ENV_1': '\"true\"',
+    'process.env.ENV_2': '\"false\"',
+    'process.env.ENV_3': '\"test\"',
+  });
+  const serverPlugins = results.overwriteServerWebpackConfig({ plugins: [] }).plugins;
+  expect(serverPlugins.length).toBe(1);
+  expect(serverPlugins[0].spec).toEqual({
+    'process.env.ENV_1': '\"true\"',
+    'process.env.ENV_2': '\"false\"',
+    'process.env.ENV_3': '\"test\"',
+  });
+});
---
+++ packages/gluestick-env-expose-ast/index.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+const detectEnvVars = require('./detectEnvironmentVariables');
+
+module.exports = (options, { logger }) => {
+  let envToExpose = [];
+  if (Array.isArray(options.parse)) {
+    envToExpose = options.parse
+      .map((file) => detectEnvVars(path.join(process.cwd(), file), logger))
+      .reduce((prev, curr) => prev.concat(curr), []);
+  } else if (options.parse) {
+    envToExpose = detectEnvVars(
+      path.join(process.cwd(), options.parse || 'src/config/application.js'),
+      logger,
+    );
+  }
+
+  const overwriteConfig = (config) => {
+    if (!envToExpose.length) {
+      return config;
+    }
+
+    config.plugins.push(
+      new webpack.DefinePlugin(
+        envToExpose.reduce((prev, curr) => {
+          return Object.assign(
+            prev,
+            { [`process.env.${curr}`]: JSON.stringify(process.env[curr]) }
+          );
+        }, {})
+      ),
+    );
+    return config;
+  };
+
+  return {
+    overwriteClientWebpackConfig: overwriteConfig,
+    overwriteServerWebpackConfig: overwriteConfig,
+  };
+};
