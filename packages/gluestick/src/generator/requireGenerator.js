84c5c678a9b0f839fa6b5c225b9a15be0cc02a5a
lukewalczak <lukasz.walczak@callstack.io>
2017-02-23 18:25:58 +0100

    Removed useless code lines

--- packages/gluestick/src/generator/requireGenerator.js
+++ packages/gluestick/src/generator/requireGenerator.js
@@ -11,14 +11,6 @@ module.createTemplate = createTemplate;
 const PREDEFINED: string = 'predefined';
 const EXTERNAL: string = 'generators';
 
-// const convertToCamelCase =
-//   (value: string): string => value.replace(/(-\w)/g, match => match[1].toUpperCase());
-//
-// const convertToKebabCase = (value: string): string => {
-//   const parsedValue = value.replace(/([A-Z])/g, match => `-${match[0].toLowerCase()}`);
-//   return parsedValue[0] === '-' ? parsedValue.substring(1) : parsedValue;
-// };
-
 const getPossiblePaths = (generatorName: string): string[] => [
   path.join(process.cwd(), `generators/${convertToCamelCase(generatorName)}.js`),
   path.join(process.cwd(), `generators/${convertToKebabCase(generatorName)}.js`),
