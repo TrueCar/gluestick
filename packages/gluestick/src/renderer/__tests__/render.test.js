a9f797d63344ce8f99d4d8dcec08b75c2e1df643
dratwas <drapich.piotr@gmail.com>
2017-02-23 12:34:17 +0100

    add flow and other case to test

--- packages/gluestick/src/renderer/__tests__/render.test.js
+++ packages/gluestick/src/renderer/__tests__/render.test.js
@@ -1,6 +1,11 @@
+/* @flow */
+import type { Logger, Config, Context, Request, GSConfig, UniversalSettings, WebpackConfig, CompiledConfig } from '../../types';
+
 const React = require('react');
 const clone = require('clone');
 const render = require('../render');
+
+type RenderMethod = (root: Object, styleTags: Object[]) => { body: string; head: Object[] };
 
 class Index extends React.Component {
   render() {
@@ -25,8 +30,68 @@ class BodyWrapper extends React.Component {
 }
 
 describe('renderer/render', () => {
-  const context = {};
-  const request = { headers: { 'user-agent': 'Moznota Browser 1.0' } };
+  const logger: Logger = {
+    success: () => {},
+    info: () => {},
+    warn: () => {},
+    debug: () => {},
+    error: () => {},
+  };
+
+  const gsConfig: GSConfig = {
+    protocol: '',
+    host: '',
+    ports: {
+      client: 0,
+      server: 0,
+    },
+    buildAssetsPath: '',
+    assetsPath: '',
+    sourcePath: '',
+    sharedPath: '',
+    appsPath: '',
+    configPath: '',
+    entryWrapperPath: '',
+    clientEntryInitPath: '',
+    serverEntriesPath: '',
+    entriesPath: '',
+    webpackChunks: '',
+    proxyLogLevel: '',
+    debugWatchDirectories: [],
+    defaultErrorTemplatePath: '',
+    customErrorTemplatePath: '',
+    autoUpgrade: {
+      added: [],
+      changed: [],
+    },
+  };
+
+  const universalSettings: UniversalSettings = {
+    server: {
+      input: '',
+      output: '',
+    },
+  };
+
+  const client: WebpackConfig = {};
+  const server: WebpackConfig = {};
+
+  const webpackConfig: CompiledConfig = {
+    universalSettings,
+    client,
+    server,
+  };
+
+  const config: Config = {
+    GSConfig: gsConfig,
+    webpackConfig,
+    plugins: [],
+  };
+
+  const context: Context = { config, logger };
+
+  const request: Request = { headers: { 'user-agent': 'Moznota Browser 1.0' }, url: '', hostname: '' };
+
   const store = {
     getState: jest.fn(() => {}),
   };
@@ -43,7 +108,7 @@ describe('renderer/render', () => {
   const assets = {};
   const cacheManager = { setCacheIfProd: jest.fn() };
 
-  describe('without a custom render method', () => {
+  describe('without a custom render method and cache set to true', () => {
     describe('when the route is an email route', () => {
       it('should render output', async () => {
         cacheManager.setCacheIfProd.mockReset();
@@ -89,12 +154,12 @@ describe('renderer/render', () => {
     });
   });
 
-  describe('with a custom render method', () => {
-    const renderMethod = (component, styleTags) => {
+  describe('with a custom render method and cache set to true', () => {
+    const renderMethod: RenderMethod = (component, styleTags) => {
       expect(component).toBeDefined();
       expect(styleTags).toBeDefined();
       return {
-        head: (<meta name="hi" />),
+        head: [<meta name="hi" />],
         body: '<div>That body!</div>',
       };
     };
@@ -138,6 +203,101 @@ describe('renderer/render', () => {
         expect(results.responseString).toBeDefined();
         expect(results.rootElement.props.head).not.toBeNull();
         expect(cacheManager.setCacheIfProd.mock.calls.length).toBe(1);
+      });
+    });
+  });
+
+  describe('without a custom render method and cache set to false', () => {
+    describe('when the route is an email route', () => {
+      it('should render output', async () => {
+        cacheManager.setCacheIfProd.mockReset();
+        const currentRouteWithEmail = clone(renderProps);
+        currentRouteWithEmail.email = true;
+        const results = await render(
+          context,
+          request,
+          { EntryPoint: Index, entryName: 'main', store, routes: getRoutes, httpClient },
+          { renderProps, currentRoute: currentRouteWithEmail },
+          { EntryWrapper, BodyWrapper, entryWrapperConfig, envVariables },
+          { assets, cacheManager },
+          {},
+        );
+        expect(results.responseString).toBeDefined();
+        expect(results.rootElement.props.head).toBeNull();
+        expect(results.rootElement.props.body.props.html).toBeDefined();
+        expect(results.rootElement.props.body.props.html).not.toContain('data-reactid');
+        expect(cacheManager.setCacheIfProd.mock.calls.length).toBe(0);
+      });
+    });
+
+    describe('when the route is not an email route', () => {
+      it('should render output', async () => {
+        cacheManager.setCacheIfProd.mockReset();
+        const currentRouteWithoutCache = clone(renderProps);
+        const results = await render(
+          context,
+          request,
+          { EntryPoint: Index, entryName: 'main', store, routes: getRoutes, httpClient },
+          { renderProps, currentRoute: currentRouteWithoutCache },
+          { EntryWrapper, BodyWrapper, entryWrapperConfig, envVariables },
+          { assets, cacheManager },
+          {},
+        );
+        expect(results.responseString).toBeDefined();
+        expect(results.rootElement.props.head).not.toBeNull();
+        expect(results.rootElement.props.body.props.html).toContain('data-reactid');
+        expect(cacheManager.setCacheIfProd.mock.calls.length).toBe(0);
+      });
+    });
+  });
+
+  describe('with a custom render method and cache set to false', () => {
+    const renderMethod: RenderMethod = (component, styleTags) => {
+      expect(component).toBeDefined();
+      expect(styleTags).toBeDefined();
+      return {
+        head: [(<meta name="hi" />)],
+        body: '<div>That body!</div>',
+      };
+    };
+
+    describe('when the route is an email route', () => {
+      it('should render output', async () => {
+        cacheManager.setCacheIfProd.mockReset();
+        const currentRouteWithEmail = clone(renderProps);
+        currentRouteWithEmail.email = true;
+        const results = await render(
+          context,
+          request,
+          { EntryPoint: Index, entryName: 'main', store, routes: getRoutes, httpClient },
+          { renderProps, currentRoute: currentRouteWithEmail },
+          { EntryWrapper, BodyWrapper, entryWrapperConfig, envVariables },
+          { assets, cacheManager },
+          { renderMethod },
+        );
+        expect(results.responseString).toBeDefined();
+        expect(results.rootElement.props.head).toBeNull();
+        expect(results.rootElement.props.body.props.html).toBeDefined();
+        expect(cacheManager.setCacheIfProd.mock.calls.length).toBe(0);
+      });
+    });
+
+    describe('when the route is not an email route', () => {
+      it('should render output', async () => {
+        cacheManager.setCacheIfProd.mockReset();
+        const currentRouteWithoutCache = clone(renderProps);
+        const results = await render(
+          context,
+          request,
+          { EntryPoint: Index, entryName: 'main', store, routes: getRoutes, httpClient },
+          { renderProps, currentRoute: currentRouteWithoutCache },
+          { EntryWrapper, BodyWrapper, entryWrapperConfig, envVariables },
+          { assets, cacheManager },
+          { renderMethod },
+        );
+        expect(results.responseString).toBeDefined();
+        expect(results.rootElement.props.head).not.toBeNull();
+        expect(cacheManager.setCacheIfProd.mock.calls.length).toBe(0);
       });
     });
   });
