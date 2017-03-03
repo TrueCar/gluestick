aed32942037ee3e60fe6dc9518181cd3b1917406
lukewalczak <lukasz.walczak@callstack.io>
2017-03-03 15:49:09 +0100

    Refactor reset-hard command

--- packages/gluestick-cli/reset.js
+++ packages/gluestick-cli/reset.js
@@ -1,10 +1,20 @@
 const { execSync } = require('child_process');
+const path = require('path');
+
+let gsDeps = null;
+let gsPackages = [];
+
+gsDeps = require(path.join(process.cwd(), 'package.json')).dependencies;
+gsPackages = Object.keys(gsDeps).filter(
+  (e => /^gluestick/.test(e) && !/\d+\.\d+\.\d+.*/.test(gsDeps[e])));
 
 module.exports = () => {
-  execSync(
-    'rm -rf node_modules/gluestick',
-    { stdio: 'inherit' },
-  );
+  gsPackages.forEach(e => {
+    execSync(
+      `rm -rf node_modules/${e}`,
+      { stdio: 'inherit' },
+    );
+  });
   execSync(
     'rm -rf build',
     { stdio: 'inherit' },
