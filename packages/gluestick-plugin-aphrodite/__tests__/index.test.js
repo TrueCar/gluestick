70d7c7de7a93d4ad5728e86c842b59d522cf2658
dratwas <drapich.piotr@gmail.com>
2017-03-03 15:20:02 +0100

    add aphrodite plugin

---
+++ packages/gluestick-plugin-aphrodite/__tests__/index.test.js
@@ -0,0 +1,13 @@
+import gluestickPluginAphrodite from '../index.js';
+import { StyleSheet, css } from "aphrodite";
+import React from 'react';
+
+const styles = StyleSheet.create({ red: { backgroundColor: 'red' } });
+const element = <div className={css(styles.red)}>DIV</div>;
+
+test('Aphrodite plugin should return body, styles and rehydrate script', () => {
+  const results = gluestickPluginAphrodite(element);
+  expect(results.body).toContain('DIV');
+  expect(results.head).not.toBeNull();
+  expect(results.additionalScript).not.toBeNull();
+});
---
+++ packages/gluestick-plugin-aphrodite/index.js
@@ -0,0 +1,13 @@
+import { StyleSheetServer } from 'aphrodite';
+import { renderToString } from 'react-dom/server';
+import React from 'react';
+
+export default (root) => {
+  const { css, html } = StyleSheetServer.renderStatic(() => {
+    return renderToString(root);
+  });
+  const head = [<style key="aphrodite-styles" data-aphrodite>{`${css.content}`}</style>];
+  const rehydrate = `window.renderedClassNames = ${JSON.stringify(css.renderedClassNames)};`;
+  const additionalScript = [(<script key="aphrodite-rehydrate" dangerouslySetInnerHTML={{ __html: rehydrate }} />)];
+  return { body: html, head, additionalScript };
+};
