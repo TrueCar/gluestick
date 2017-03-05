1f2b1e0a12807af561a9075c91983723aeacec2d
Paweł Trysła <pawel.trysla@gmail.com>
2017-03-05 18:40:08 +0100

    remove old files

--- packages/gluestick/src/plugins/pluginsFilter.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-
-module.exports = (plugins: Array<Object>, flag: string, invert: boolean = false): Array<Object> => {
-  return plugins.filter((plugin) => {
-    const baseCheck = typeof plugin !== 'string' && plugin[flag];
-    return invert ? !baseCheck : baseCheck;
-  });
-};
