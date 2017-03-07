d1dd0a2a7f2c111e791f2ee922987fcd2cac4d7e
Michal Chudziak <michal.chudziak@gmail.com>
2017-03-07 15:20:01 +0100

    Fix production server start.

--- packages/gluestick/src/commands/utils.js
+++ packages/gluestick/src/commands/utils.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 module.exports = {
-  filterArg: (args: string[], argToExclude: string): string[] => {
-    return args.filter(arg => arg !== `--${argToExclude}`);
+  filterArg: (args: string[], argsToExclude: string[]): string[] => {
+    return args.filter(arg => !argsToExclude.includes(arg));
   },
 };
