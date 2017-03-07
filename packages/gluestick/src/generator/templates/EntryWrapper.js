787f8bff9d02138f11519441646dd1fd2d648aaf
dratwas <drapich.piotr@gmail.com>
2017-03-07 12:10:54 +0100

    call hooks from plugins

--- packages/gluestick/src/generator/templates/EntryWrapper.js
+++ packages/gluestick/src/generator/templates/EntryWrapper.js
@@ -15,7 +15,7 @@ import browserHistory from "react-router/lib/browserHistory";
 const start = (
   getRoutes,
   getStore,
-  { rootWrappers, rootWrappersOptions } = {},
+  { rootWrappers, rootWrappersOptions, hooks } = {},
   match = originalMatch,
   history = browserHistory
 ) => {
@@ -40,6 +40,9 @@ const start = (
         {...renderProps}
       />
     );
+    if (hooks) {
+      hooks.forEach((hook) => { hook(); });
+    }
     render(entry, document.getElementById("main"));
   });
 };
