71f6f40fc227df8371f78be930a0e5ae95749c2a
dratwas <drapich.piotr@gmail.com>
2017-03-07 15:28:24 +0100

    make aphrodite plugin works

--- packages/gluestick-plugin-aphrodite/__tests__/index.test.js
+++ packages/gluestick-plugin-aphrodite/__tests__/index.test.js
@@ -1,12 +1,12 @@
 import { StyleSheet, css } from 'aphrodite';
 import React from 'react';
-import gluestickPluginAphrodite from '../index.js';
+import gluestickPluginAphrodite from '../server.js';
 
 const styles = StyleSheet.create({ red: { backgroundColor: 'red' } });
 const element = <div className={css(styles.red)}>DIV</div>;
 
 test('Aphrodite plugin should return body, styles and rehydrate script', () => {
-  const results = gluestickPluginAphrodite(element);
+  const results = gluestickPluginAphrodite().renderMethod(element);
   expect(results.body).toContain('DIV');
   expect(results.head).not.toBeNull();
   expect(results.additionalScript).not.toBeNull();
