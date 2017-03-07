a0675bde2971d33809607c71df3d40b61008ee3e
Ferran Negre <fnp.developer@gmail.com>
2017-03-07 21:42:22 +0100

    Update main route for new generated apps (#755)
    * Fix production server start. (#746)
    
    * Fix production server start.
    
    * Fix lint errors.
    
    * Update runWithPM2.js
    
    * Update utils.test.js
    
    * Update start.js
    
    * #735: Update main route for new generated apps.
    
    * #735: Simpler condition.

--- packages/gluestick/src/generator/predefined/app.js
+++ packages/gluestick/src/generator/predefined/app.js
@@ -20,96 +20,102 @@ const templateNoMatchApp = require('../templates/NoMatchApp')(createTemplate);
 const templateReducer = require('../templates/Reducer')(createTemplate);
 const { convertToCamelCase, convertToKebabCase } = require('../utils');
 
-module.exports = (options: GeneratorOptions) => ({
-  modify: {
-    file: 'src/entries.json',
-    modifier: (content: string) => {
-      const entries = content ? JSON.parse(content) : {};
-      entries[`/${convertToKebabCase(options.name)}`] = {
-        name: convertToCamelCase(options.name),
-        component: `src/apps/${options.name}/Index.js`,
-        routes: `src/apps/${options.name}/routes.js`,
-        reducers: `src/apps/${options.name}/reducers`,
-      };
-      return JSON.stringify(entries, null, '  ');
+module.exports = (options: GeneratorOptions) => {
+  const appName = convertToKebabCase(options.name);
+  return ({
+    modify: {
+      file: 'src/entries.json',
+      modifier: (content: string) => {
+        const entries = content ? JSON.parse(content) : {};
+        entries[`/${appName}`] = {
+          name: convertToCamelCase(options.name),
+          component: `src/apps/${options.name}/Index.js`,
+          routes: `src/apps/${options.name}/routes.js`,
+          reducers: `src/apps/${options.name}/reducers`,
+        };
+        return JSON.stringify(entries, null, '  ');
+      },
     },
-  },
-  entries: [
-// Your new inner app
-    {
-      path: `src/apps/${options.name}`,
-      filename: 'routes.js',
-      template: templateRoutes,
-    },
-    {
-      path: `src/apps/${options.name}`,
-      filename: 'Index.js',
-      template: templateIndex,
-    },
-    {
-      path: `src/apps/${options.name}/components`,
-      filename: 'Home.js',
-      template: templateHome,
-    },
-    {
-      path: `src/apps/${options.name}/components`,
-      filename: 'Home.css',
-      template: templateHomeCss,
-    },
-    {
-      path: `src/apps/${options.name}/components`,
-      filename: 'MasterLayout.js',
-      template: templateMasterLayout,
-    },
-    {
-      path: `src/apps/${options.name}/containers`,
-      filename: 'HomeApp.js',
-      template: templateHomeApp,
-    },
-    {
-      path: `src/apps/${options.name}/containers`,
-      filename: 'NoMatchApp.js',
-      template: templateNoMatchApp,
-    },
-    {
-      path: `src/apps/${options.name}/actions`,
-      filename: '.gitkeep',
-      template: templateEmpty,
-    },
-    {
-      path: `src/apps/${options.name}/reducers`,
-      filename: 'index.js',
-      template: templateReducer,
-    },
-    {
-      path: `src/apps/${options.name}/components/__tests__`,
-      filename: 'Home.test.js',
-      template: templateHomeTest,
-    },
-    {
-      path: `src/apps/${options.name}/components/__tests__`,
-      filename: 'MasterLayout.test.js',
-      template: templateMasterLayoutTest,
-    },
-    {
-      path: `src/apps/${options.name}/containers/__tests__`,
-      filename: 'HomeApp.test.js',
-      template: templateContainerHomeTest,
-    },
-    {
-      path: `src/apps/${options.name}/containers/__tests__`,
-      filename: 'NoMatchApp.test.js',
-      template: templateNoMatchAppTest,
-    },
-    {
-      path: `src/apps/${options.name}/reducers/__tests__`,
-      filename: '.gitkeep',
-      template: templateEmpty,
-    },
-    {
-      path: `src/apps/${options.name}/actions/__tests__`,
-      filename: '.gitkeep',
-      template: templateEmpty,
-    },
-  ],
-});
+    entries: [
+      // Your new inner app
+      {
+        path: `src/apps/${options.name}`,
+        filename: 'routes.js',
+        template: templateRoutes,
+        args: {
+          index: appName,
+        },
+      },
+      {
+        path: `src/apps/${options.name}`,
+        filename: 'Index.js',
+        template: templateIndex,
+      },
+      {
+        path: `src/apps/${options.name}/components`,
+        filename: 'Home.js',
+        template: templateHome,
+      },
+      {
+        path: `src/apps/${options.name}/components`,
+        filename: 'Home.css',
+        template: templateHomeCss,
+      },
+      {
+        path: `src/apps/${options.name}/components`,
+        filename: 'MasterLayout.js',
+        template: templateMasterLayout,
+      },
+      {
+        path: `src/apps/${options.name}/containers`,
+        filename: 'HomeApp.js',
+        template: templateHomeApp,
+      },
+      {
+        path: `src/apps/${options.name}/containers`,
+        filename: 'NoMatchApp.js',
+        template: templateNoMatchApp,
+      },
+      {
+        path: `src/apps/${options.name}/actions`,
+        filename: '.gitkeep',
+        template: templateEmpty,
+      },
+      {
+        path: `src/apps/${options.name}/reducers`,
+        filename: 'index.js',
+        template: templateReducer,
+      },
+      {
+        path: `src/apps/${options.name}/components/__tests__`,
+        filename: 'Home.test.js',
+        template: templateHomeTest,
+      },
+      {
+        path: `src/apps/${options.name}/components/__tests__`,
+        filename: 'MasterLayout.test.js',
+        template: templateMasterLayoutTest,
+      },
+      {
+        path: `src/apps/${options.name}/containers/__tests__`,
+        filename: 'HomeApp.test.js',
+        template: templateContainerHomeTest,
+      },
+      {
+        path: `src/apps/${options.name}/containers/__tests__`,
+        filename: 'NoMatchApp.test.js',
+        template: templateNoMatchAppTest,
+      },
+      {
+        path: `src/apps/${options.name}/reducers/__tests__`,
+        filename: '.gitkeep',
+        template: templateEmpty,
+      },
+      {
+        path: `src/apps/${options.name}/actions/__tests__`,
+        filename: '.gitkeep',
+        template: templateEmpty,
+      },
+    ],
+  });
+};
