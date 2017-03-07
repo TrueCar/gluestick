25b5e9aeb0d0ee2f84282c8db80d03c70984ce5a
Paweł Trysła <pawel.trysla@gmail.com>
2017-03-07 12:13:29 +0100

    git push

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,8 +3,9 @@
 const spawn = require('cross-spawn').spawn;
 
 // Publish packages to npm registry
-spawn('npm', [
+spawn.sync('npm', [
   'run', 'lerna', 'publish', '--', '--repo-version', process.argv[2], '--yes', '--skip-npm',
 ], { stdio: 'inherit' });
+spawn.sync('git', ['push']);
 // Create docker image and push to Docker Hub
 require('./docker/create-base-image');
