81e103d19766be29b3267494b1fec55565b79a3d
Paweł Trysła <pawel.trysla@gmail.com>
2017-03-07 16:06:36 +0100

    fix typos

--- packages/gluestick/bin/gluestick.js
+++ packages/gluestick/bin/gluestick.js
@@ -3,9 +3,9 @@
 if (!__dirname.startsWith(process.cwd())) {
   const red = require('chalk').red;
   console.log(red('It look like you\'ve installed `gluestick` package globally,'));
-  console.log(red('but `gluestick` is ment to be installed locally as a dependency.'));
+  console.log(red('but `gluestick` is meant to be installed locally as a dependency.'));
   console.log(red('To ensure everything works correctly, please uninstall `gluestick`'));
-  console.log(red('and install globally `gluestick-cli`.'));
+  console.log(red('and install (globally) `gluestick-cli` instead.'));
   process.exit(0);
 }
 
