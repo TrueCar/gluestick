53b5375fa0da22f2fa621c444cf8aef4962b8d2c
Paweł Trysła <pawel.trysla@gmail.com>
2017-03-08 12:22:46 +0100

    remove manual gluestick-* deps updating

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,9 +7,6 @@ if (process.argv.length <= 2 || !/\d+\.\d+\.\d+.*/.test(process.argv[2])) {
 
 const version = process.argv[2];
 const spawn = require('cross-spawn');
-const glob = require('glob');
-const path = require('path');
-const fs = require('fs');
 
 const spawnWithErrorHandling = (...args) => {
   const results = spawn.sync(...args);
@@ -17,30 +14,6 @@ const spawnWithErrorHandling = (...args) => {
     throw results.error;
   }
 };
-
-// Update gluestick-* dependecies in packages
-const packages = glob.sync('./packages/*', { cwd: process.cwd() });
-const updateGluestickDeps = (deps) => {
-  const updatedDeps = deps;
-  packages.forEach((packagePath) => {
-    const name = path.basename(packagePath);
-    if (updatedDeps[name]) {
-      updatedDeps[name] = version;
-    }
-  });
-  return updatedDeps;
-};
-packages.forEach((packagePath) => {
-  const filePath = path.join(process.cwd(), packagePath, 'package.json');
-  const packageJSON = require(filePath);
-  if (packageJSON.dependencies) {
-    packageJSON.dependencies = updateGluestickDeps(packageJSON.dependencies);
-  }
-  if (packageJSON.devDependencies) {
-    packageJSON.devDependencies = updateGluestickDeps(packageJSON.devDependencies);
-  }
-  fs.writeFileSync(filePath, `${JSON.stringify(packageJSON, null, '  ')}\n`, 'utf-8');
-});
 
 // Publish packages to npm registry
 spawnWithErrorHandling('npm', [
