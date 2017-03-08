37a7f7f673fc88aa6ac337546bd02c2b48b717c2
Paweł Trysła <pawel.trysla@gmail.com>
2017-03-08 10:53:23 +0100

    improve error handling

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,17 +5,25 @@ if (process.argv.length <= 2 || !/\d+\.\d+\.\d+.*/.test(process.argv[2])) {
   process.exit(1);
 }
 
+const version = process.argv[2];
 const spawn = require('cross-spawn').spawn;
 
+const spawnWithErrorHandling = (...args) => {
+  const results = spawn.sync(...args);
+  if (results.error) {
+    throw results.error;
+  }
+};
+
 // Publish packages to npm registry
-spawn.sync('npm', [
-  'run', 'lerna', 'publish', '--', '--repo-version', process.argv[2], '--yes',
+spawnWithErrorHandling('npm', [
+  'run', 'lerna', 'publish', '--', '--repo-version', version, '--yes',
 ], { stdio: 'inherit' });
 
 console.log('Pushing tag commit...');
-spawn.sync('git', ['push']);
+spawnWithErrorHandling('git', ['push']);
 
 // Create docker image and push to Docker Hub
-require('./docker/create-base-image');
+require('./docker/create-base-image')(spawnWithErrorHandling);
 
 console.log('Done!');
--- scripts/docker/create-base-image.js
+++ scripts/docker/create-base-image.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 
-const spawn = require('cross-spawn').spawn;
+const crossSpawn = require('cross-spawn');
 const version = require('../../lerna.json').version;
 
 const tag = `truecar/gluestick:${version}`;
 
-console.log('Building docer image...');
-console.log(spawn.sync('docker', [
-  'build',
-  '-f', './scripts/docker/Dockerfile',
-  '--force-rm=true',
-  '-t', tag,
-  '--build-arg', `GLUESTICK_VERSION=${version}`,
-  '.',
-], { stdio: 'inherit', env: Object.assign({}, process.env) }));
+module.exports = (spawn = crossSpawn) => {
+  console.log('Building docer image...');
+  console.log(spawn('docker', [
+    'build',
+    '-f', './scripts/docker/Dockerfile',
+    '--force-rm=true',
+    '-t', tag,
+    '--build-arg', `GLUESTICK_VERSION=${version}`,
+    '.',
+  ], { stdio: 'inherit', env: Object.assign({}, process.env) }));
 
-console.log('Pushing image to Docker Hub...');
-spawn.sync('docker', ['push', tag], { stdio: 'inherit', env: Object.assign({}, process.env) });
+  console.log('Pushing image to Docker Hub...');
+  spawn('docker', ['push', tag], { stdio: 'inherit', env: Object.assign({}, process.env) });
+};
