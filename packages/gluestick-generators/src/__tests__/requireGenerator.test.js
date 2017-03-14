0d6d544173ce8356e06cf28b892de6cddeb11e2b
Paweł Trysła <pawel.trysla@gmail.com>
2017-03-15 00:24:33 +0100

    imporve coverage in gluestick-generators part 2

--- packages/gluestick-generators/src/__tests__/requireGenerator.test.js
+++ packages/gluestick-generators/src/__tests__/requireGenerator.test.js
@@ -1,14 +1,31 @@
-const fsMock = {};
+let fsMock = {};
 jest.setMock('fs', {
   existsSync: path => typeof fsMock[path] !== 'undefined',
 });
+jest.mock('cwd/generators/test.js', () => 'test', { virtual: true });
 
+const path = require('path');
 const requireGenerator = require('../requireGenerator');
 
 describe('generator/requireGenerator', () => {
+  beforeEach(() => {
+    fsMock = {};
+  });
+
   it('should thow error if generator config was not found', () => {
     expect(() => {
       requireGenerator('Test');
     }).toThrowError();
+  });
+
+  it('should return generator config', () => {
+    const originalProcessCwd = process.cwd.bind(process);
+    process.cwd = () => 'cwd';
+    fsMock[path.join(process.cwd(), 'generators', 'test.js')] = 'test';
+    expect(requireGenerator('test')).toEqual({
+      name: 'test',
+      config: 'test',
+    });
+    process.cwd = originalProcessCwd;
   });
 });
