28e876073548876ff1ab8d96e93fbfe4e1a8347f
Paweł Trysła <pawel.trysla@gmail.com>
2017-03-24 15:34:38 +0100

    remove double check if command is in gluestick project

--- packages/gluestick-bin/src/commands/new.js
+++ packages/gluestick-bin/src/commands/new.js
@@ -28,32 +28,19 @@ const generateTemplate = (
   }, logger);
 };
 
-const currentlyInProjectFolder = (folderPath: string) => {
-  const fileName: string = path.join(folderPath, 'package.json');
-  let data: ?ProjectData = null;
-  try {
-    data = require(fileName);
-    return !!data.dependencies && !!data.dependencies.gluestick;
-  } catch (e) {
-    return false;
-  }
-};
-
 module.exports = ({ logger }: Context, appName: string, options: Object = {}) => {
-  if (currentlyInProjectFolder(process.cwd())) {
-    logger.info(`${filename(appName)} is being generated...`);
+  logger.info(`${filename(appName)} is being generated...`);
 
-    generateTemplate('new', appName, logger, { dev: options.dev || null, appName });
+  generateTemplate('new', appName, logger, { dev: options.dev || null, appName });
 
-    // Install necessary flow-typed definitions
-    spawn.sync('./node_modules/.bin/flow-typed', ['install', `jest@${packageJSON.dependencies.jest}`], { stdio: 'inherit' });
+  // Install necessary flow-typed definitions
+  spawn.sync('./node_modules/.bin/flow-typed', ['install', `jest@${packageJSON.dependencies.jest}`], { stdio: 'inherit' });
 
-    logger.info(`${highlight('New GlueStick project created')} at ${filename(process.cwd())}`);
-    logger.info('To run your app and start developing');
-    logger.info(`    cd ${appName}`);
-    logger.info('    gluestick start');
-    logger.info('    Point the browser to http://localhost:8888');
+  logger.info(`${highlight('New GlueStick project created')} at ${filename(process.cwd())}`);
+  logger.info('To run your app and start developing');
+  logger.info(`    cd ${appName}`);
+  logger.info('    gluestick start');
+  logger.info('    Point the browser to http://localhost:8888');
 
-    process.exit(0);
-  }
+  process.exit(0);
 };
