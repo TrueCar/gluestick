6907102dedef0fa4d9e84ac1f7d5c8d97aaf3632
Paweł Trysła <pawel.trysla@gmail.com>
2017-03-30 16:08:18 +0200

    fix lint

--- packages/gluestick/src/commands/start-server/runWithWebpack.js
+++ packages/gluestick/src/commands/start-server/runWithWebpack.js
@@ -10,35 +10,17 @@ const logMessage = require('./logMessage');
 
 function debounce(func, wait, immediate) {
   let timeout;
-
-  // Calling debounce returns a new anonymous function
   return (...args) => {
-    // Should the function be called now? If immediate is true
-    //   and not already in a timeout then the answer is: Yes
     const callNow = immediate && !timeout;
-
-    // This is the basic debounce behaviour where you can call this 
-    //   function several times, but it will only execute once 
-    //   [before or after imposing a delay]. 
-    //   Each time the returned function is called, the timer starts over.
     clearTimeout(timeout);
 
-    // Set the new timeout
     timeout = setTimeout(() => {
-      // Inside the timeout function, clear the timeout variable
-      // which will let the next execution run when in 'immediate' mode
       timeout = null;
-
-      // Check if the function already ran with the immediate flag
       if (!immediate) {
-        // Call the original function with apply
-        // apply lets you define the 'this' object as well as the arguments 
-        //    (both captured before setTimeout)
         func(...args);
       }
     }, wait);
 
-    // Immediate mode and no wait timer? Execute the function..
     if (callNow) func(...args);
   };
 }
