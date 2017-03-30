b7181378330d694236cc8fe2581f7604b81db526
Paweł Trysła <pawel.trysla@gmail.com>
2017-03-30 16:06:32 +0200

    debounce compilations

--- packages/gluestick/src/commands/start-server/runWithWebpack.js
+++ packages/gluestick/src/commands/start-server/runWithWebpack.js
@@ -8,6 +8,41 @@ const path = require('path');
 const { filename } = require('../../cli/colorScheme');
 const logMessage = require('./logMessage');
 
+function debounce(func, wait, immediate) {
+  let timeout;
+
+  // Calling debounce returns a new anonymous function
+  return (...args) => {
+    // Should the function be called now? If immediate is true
+    //   and not already in a timeout then the answer is: Yes
+    const callNow = immediate && !timeout;
+
+    // This is the basic debounce behaviour where you can call this 
+    //   function several times, but it will only execute once 
+    //   [before or after imposing a delay]. 
+    //   Each time the returned function is called, the timer starts over.
+    clearTimeout(timeout);
+
+    // Set the new timeout
+    timeout = setTimeout(() => {
+      // Inside the timeout function, clear the timeout variable
+      // which will let the next execution run when in 'immediate' mode
+      timeout = null;
+
+      // Check if the function already ran with the immediate flag
+      if (!immediate) {
+        // Call the original function with apply
+        // apply lets you define the 'this' object as well as the arguments 
+        //    (both captured before setTimeout)
+        func(...args);
+      }
+    }, wait);
+
+    // Immediate mode and no wait timer? Execute the function..
+    if (callNow) func(...args);
+  };
+}
+
 /**
  * Spawns new process with rendering server.
  *
@@ -40,11 +75,11 @@ module.exports = ({ config, logger }: Context, entryPointPath: string, args: str
   const webpackConfig: WebpackConfigEntry = config.webpackConfig.server;
   let child: ?Object = null;
   const compile = (): void => {
+    logger.info('Building server entry.');
     webpack(webpackConfig).run(error => {
       if (error) {
         throw error;
       }
-      logger.info('Building server entry.');
       if (child) {
         child.kill();
       }
@@ -53,6 +88,9 @@ module.exports = ({ config, logger }: Context, entryPointPath: string, args: str
   };
   logger.debug('Initial compilation');
   compile();
+  const debouncedCompile = debounce(() => {
+    compile();
+  }, 10000);
   const watcher: Object = chokidar.watch([
     path.join(process.cwd(), '**/*'),
   ], {
@@ -60,13 +98,13 @@ module.exports = ({ config, logger }: Context, entryPointPath: string, args: str
       /\.DS_Store/,
       /build\/server/,
       /gluestick\/clientEntryInit/,
-      /node_modules\/(?!gluestick\/src)/,
+      /node_modules\/(?!gluestick\/src\/renderer)/,
     ],
   }).on('ready', () => {
     logger.info('Started watching...');
     watcher.on('all', (stat, file) => {
       logger.info(`File ${filename(file)} changed [${stat}]`);
-      compile();
+      debouncedCompile();
     });
   });
 };
