715a65cb168daed1d0d7dab4bf9f571204135ded
lukewalczak <lukasz.walczak@callstack.io>
2017-04-03 15:47:39 +0200

    Gluestick require version from npm

--- packages/gluestick-cli/new.js
+++ packages/gluestick-cli/new.js
@@ -6,85 +6,91 @@ const commander = require('commander');
 const glob = require('glob');
 const chalk = require('chalk');
 const generate = require('gluestick-generators').default;
-const version = require('./package.json').version;
+const fetch = require('node-fetch');
+
+
 
 module.exports = (appName, options, exitWithError) => {
-  const packageDeps = {
-    dependencies: {
-      gluestick: version,
-    },
-  };
-  if (options.dev) {
-    const pathToGluestickRepo = path.join(process.cwd(), appName, '..', options.dev);
-    const pathToGluestickPackages = path.join(pathToGluestickRepo, 'packages');
-    let gluestickPackage = {};
-    const packages = glob.sync('*', { cwd: pathToGluestickPackages }).filter((e) => e !== 'gluestick-cli');
-    try {
-      gluestickPackage = require(path.join(pathToGluestickRepo, 'package.json'));
-    } catch (error) {
-      exitWithError(
-        `Development GlueStick path ${pathToGluestickRepo} is not valid`,
+  fetch('http://registry.npmjs.org/gluestick')
+    .then(res => res.json())
+    .then(json => {
+      const packageDeps = {
+        dependencies: {
+          gluestick: json['dist-tags'].latest,
+        },
+      };
+      if (options.dev) {
+        const pathToGluestickRepo = path.join(process.cwd(), appName, '..', options.dev);
+        const pathToGluestickPackages = path.join(pathToGluestickRepo, 'packages');
+        let gluestickPackage = {};
+        const packages = glob.sync('*', { cwd: pathToGluestickPackages }).filter((e) => e !== 'gluestick-cli');
+        try {
+          gluestickPackage = require(path.join(pathToGluestickRepo, 'package.json'));
+        } catch (error) {
+          exitWithError(
+            `Development GlueStick path ${pathToGluestickRepo} is not valid`,
+          );
+        }
+        if (gluestickPackage.name !== 'gluestick-packages') {
+          exitWithError(
+            `${pathToGluestickRepo} is not a path to GlueStick`,
+          );
+        }
+        packages.forEach(e => {
+          packageDeps.dependencies[e] = `file:${path.join('..', options.dev, 'packages', e)}`;
+        });
+      }
+
+      const pathToApp = path.join(process.cwd(), appName);
+      if (fs.existsSync(pathToApp)) {
+        exitWithError(
+          `Directory ${pathToApp} already exists`,
+        );
+      }
+      mkdir.sync(path.join(process.cwd(), appName));
+
+      const generatorOptions = {
+        gluestickDependencies: packageDeps.dependencies,
+        appName,
+      };
+
+      process.chdir(appName);
+
+      generate(
+        {
+          generatorName: 'package',
+          entityName: 'package',
+          options: generatorOptions,
+        },
       );
-    }
-    if (gluestickPackage.name !== 'gluestick-packages') {
-      exitWithError(
-        `${pathToGluestickRepo} is not a path to GlueStick`,
+
+      const isYarnAvailable = !spawn.sync('yarn', ['-V']).error;
+      if (!options.npm && !isYarnAvailable) {
+        console.log(
+          chalk.yellow.bgBlack('You are installing dependencies using npm, consider using yarn.'),
+        );
+      }
+
+      spawn.sync(
+        !options.npm && isYarnAvailable ? 'yarn' : 'npm',
+        ['install'],
+        {
+          cwd: process.cwd(),
+          stdio: 'inherit',
+        },
       );
-    }
-    packages.forEach(e => {
-      packageDeps.dependencies[e] = `file:${path.join('..', options.dev, 'packages', e)}`;
+      // Remove --npm or -n options cause this is no longer needed in
+      // gluestick new command.
+      const args = commander.rawArgs.slice(2)
+        .filter((v) => v !== '--npm' && v !== '-n');
+
+      spawn.sync(
+        './node_modules/.bin/gluestick',
+        args,
+        {
+          cwd: process.cwd(),
+          stdio: 'inherit',
+        },
+      );
     });
-  }
-
-  const pathToApp = path.join(process.cwd(), appName);
-  if (fs.existsSync(pathToApp)) {
-    exitWithError(
-      `Directory ${pathToApp} already exists`,
-    );
-  }
-  mkdir.sync(path.join(process.cwd(), appName));
-
-  const generatorOptions = {
-    gluestickDependencies: packageDeps.dependencies,
-    appName,
-  };
-
-  process.chdir(appName);
-
-  generate(
-    {
-      generatorName: 'package',
-      entityName: 'package',
-      options: generatorOptions,
-    },
-  );
-
-  const isYarnAvailable = !spawn.sync('yarn', ['-V']).error;
-  if (!options.npm && !isYarnAvailable) {
-    console.log(
-      chalk.yellow.bgBlack('You are installing dependencies using npm, consider using yarn.'),
-    );
-  }
-
-  spawn.sync(
-    !options.npm && isYarnAvailable ? 'yarn' : 'npm',
-    ['install'],
-    {
-      cwd: process.cwd(),
-      stdio: 'inherit',
-    },
-  );
-  // Remove --npm or -n options cause this is no longer needed in
-  // gluestick new command.
-  const args = commander.rawArgs.slice(2)
-    .filter((v) => v !== '--npm' && v !== '-n');
-
-  spawn.sync(
-    './node_modules/.bin/gluestick',
-    args,
-    {
-      cwd: process.cwd(),
-      stdio: 'inherit',
-    },
-  );
 };
