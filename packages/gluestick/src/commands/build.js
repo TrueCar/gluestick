d317957127ca1a7523a6182addea97cb54fcbaa7
lukewalczak <lukasz.walczak@callstack.io>
2017-04-06 12:58:40 +0200

    Build function refactor

--- packages/gluestick/src/commands/build.js
+++ packages/gluestick/src/commands/build.js
@@ -4,36 +4,19 @@ import type { Context, WebpackConfig, Compiler } from '../types.js';
 const webpack = require('webpack');
 const createWebpackStats = require('../config/createWebpackStats');
 
-const buildClient = ({ logger, config } : Context, options: Object): void => {
-  const configuration: WebpackConfig = config.webpackConfig.client;
+const buildFunc = ({ logger, config } : Context, options: Object, buildType: string): void => {
+  const configuration: WebpackConfig = config.webpackConfig[buildType];
   const compiler: Compiler = webpack(configuration);
 
   compiler.run((error: string, stats: Object) => {
     if (error) {
       throw new Error(error);
     }
-    logger.success('Client bundle has been prepared for production.');
+    logger.success(`${buildType[0].toUpperCase() + buildType.slice(1)} bundle has been prepared for production.`);
 
     if (options.stats) {
       logger.info('Creating webpack stats');
-      createWebpackStats(`${config.GSConfig.webpackStats}-client`, stats);
-    }
-  });
-};
-
-const buildServer = ({ logger, config } : Context, options: Object): void => {
-  const configuration: WebpackConfig = config.webpackConfig.server;
-  const compiler: Compiler = webpack(configuration);
-
-  compiler.run((error: string, stats: Object) => {
-    if (error) {
-      throw new Error(error);
-    }
-    logger.success('Server bundle has been prepared for production.');
-
-    if (options.stats) {
-      logger.info('Creating webpack stats');
-      createWebpackStats(`${config.GSConfig.webpackStats}-server`, stats);
+      createWebpackStats(`${config.GSConfig.webpackStats}-${buildType}`, stats);
     }
   });
 };
@@ -42,16 +25,16 @@ module.exports = ({ logger, config }: Context, ...commandArgs: any[]): void => {
   const options: Object = commandArgs[commandArgs.length - 1];
   if (config.webpackConfig) {
     if (options.client) {
-      buildClient({ logger, config }, options);
+      buildFunc({ logger, config }, options, 'client');
     }
 
     if (options.server) {
-      buildServer({ logger, config }, options);
+      buildFunc({ logger, config }, options, 'server');
     }
 
     if (!options.client && !options.server) {
-      buildClient({ logger, config }, options);
-      buildServer({ logger, config }, options);
+      buildFunc({ logger, config }, options, 'client');
+      buildFunc({ logger, config }, options, 'server');
     }
   } else {
     logger.error('Webpack config not specified');
