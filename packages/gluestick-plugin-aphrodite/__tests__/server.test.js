104a30f9f1fc84eae1142b4ac683dd90c1321805
Paweł Trysła <pawel.trysla@gmail.com>
2017-04-07 12:27:02 +0200

    update test

--- packages/gluestick-plugin-aphrodite/__tests__/server.test.js
+++ packages/gluestick-plugin-aphrodite/__tests__/server.test.js
@@ -6,8 +6,8 @@ const styles = StyleSheet.create({ red: { backgroundColor: 'red' } });
 const element = <div className={css(styles.red)}>DIV</div>;
 
 test('Aphrodite plugin should return body, styles and rehydrate script', () => {
-  const results = gluestickPluginAphrodite().renderMethod(element);
+  const results = gluestickPluginAphrodite().renderMethod(element, [<link />]);
   expect(results.body).toContain('DIV');
-  expect(results.head).not.toBeNull();
+  expect(results.head.length).toBe(2);
   expect(results.additionalScript).not.toBeNull();
 });
