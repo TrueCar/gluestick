6f01158fc4d7df4692100002d5209ba29a413161
Paweł Trysła <pawel.trysla@gmail.com>
2017-04-27 11:10:25 +0200

    use peerDependencies

--- packages/gluestick-generators/templates/package.js
+++ packages/gluestick-generators/templates/package.js
@@ -17,14 +17,15 @@ const templatePackage = createTemplate`
   "dependencies": {
     "babel-core": "6.22.1",
     "babel-loader": "6.2.10",
-    "babel-plugin-gluestick": "0.0.1",
     "babel-plugin-transform-decorators-legacy": "1.3.4",
+    "babel-plugin-transform-flow-strip-types": "6.22.0",
+    "babel-polyfill": "6.23.0",
     "babel-preset-es2015": "6.22.0",
     "babel-preset-react": "6.22.0",
     "babel-preset-stage-0": "6.22.0",
-    "css-loader": "0.27.3",
+    "css-loader": "0.28.0",
     "extract-text-webpack-plugin": "2.1.0",
-    "file-loader": "0.9.0",
+    "file-loader": "0.11.1",
     ${(args) => Object.keys(args.gluestickDependencies).reverse().reduce(
       (prev, key, i, arr) => prev.concat(
         `"${key}": "${args.gluestickDependencies[key]}",${i === arr.length - 1 ? '' : '\n    '}`,
@@ -32,6 +33,10 @@ const templatePackage = createTemplate`
       '',
     )}
     "image-webpack-loader": "3.3.0",
+    "optimize-css-assets-webpack-plugin": "1.3.1",
+    "postcss-calc": "5.3.1",
+    "postcss-custom-properties": "5.0.2",
+    "postcss-loader": "1.3.3",
     "normalize.css": "^5.0.0",
     "react": "15.4.2",
     "react-dom": "15.4.2",
@@ -39,8 +44,11 @@ const templatePackage = createTemplate`
     "react-redux": "5.0.2",
     "react-router": "3.0.2",
     "redux": "3.6.0",
+    "redux-thunk": "2.2.0",
     "sass-loader": "6.0.3",
-    "style-loader": "0.16.1"
+    "style-loader": "0.16.1",
+    "url-loader": "0.5.8",
+    "webpack": "2.4.1"
   },
   "author": "",
   "license": "ISC",
