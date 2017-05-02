f22e5a54b2bcb013b788de4f13ffabced96d8671
Todd Williams <toddsurfs@icloud.com>
2017-05-01 18:10:12 -0700

    resolve docker build issues
    1. .dockerignore was not ignorring node_modules folder when comment lived
    next to it
    
    2. gluestick dockerize was running yarn but we were still not
    pre-installing it
    
    3. With the speed of yarn and the new folder structure it seemed
    reasonable to remove the comment about what we used to do in 0.10.x for
    templates/new/package.json
    
    4. We had a random `npm install` command which really wouldn't do
    anything without a package.json file existing so removed that
    
    5. Builds were failing for missing node-gyp so we install it globally
    now

--- packages/gluestick/generator/templates/dockerignore.js
+++ packages/gluestick/generator/templates/dockerignore.js
@@ -2,7 +2,7 @@
 import type { CreateTemplate } from '../../src/types';
 
 module.exports = (createTemplate: CreateTemplate) => createTemplate`
-node_modules #required for gluestick dockerizing, DO NOT REMOVE
+node_modules
 .git
 .gitignore
 `;
