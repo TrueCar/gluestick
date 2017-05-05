83cce8b025fd2e8dedbc2e234c1ebdcc51c05a78
Paweł Trysła <pawel.trysla@gmail.com>
2017-05-05 17:42:33 +0200

    update tests

--- packages/gluestick/src/renderer/helpers/__tests__/getAssetsLoader.test.js
+++ packages/gluestick/src/renderer/helpers/__tests__/getAssetsLoader.test.js
@@ -4,7 +4,16 @@ describe('renderer/helpers/getAssetsLoader', () => {
   it('should return code for assets loading in browser', () => {
     const snippet = getAssetsLoader({}, 'entryBundle.js', 'vendorBundle.js');
     expect(snippet.length).toBeGreaterThan(0);
-    expect(snippet).toContain("document.addEventListener('DOMContentLoaded', function");
+    expect(snippet).toContain('document.addEventListener(\'DOMContentLoaded\', function');
   });
-//  it('should include loadjs options', () => {} )
+
+  it('should include loadjs options', () => {
+    const snippet = getAssetsLoader(
+      { before: () => { console.log('before'); } }, 'entryBundle.js', 'vendorBundle.js',
+    );
+    expect(snippet.length).toBeGreaterThan(0);
+    expect(snippet).toContain('document.addEventListener(\'DOMContentLoaded\', function');
+    expect(snippet).toContain('document.addEventListener(\'DOMContentLoaded\', function');
+    expect(snippet).toContain('console.log(\'before\')');
+  });
 });
