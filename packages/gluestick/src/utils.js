470ded4ce1318e0780be01c615158eb64e2e3646
Paweł Trysła <pawel.trysla@gmail.com>
2017-05-29 15:12:04 +0200

    display timing info + deplicated modules

--- packages/gluestick/src/utils.js
+++ packages/gluestick/src/utils.js
@@ -123,6 +123,9 @@ const printWebpackStats = (logger: Logger, stats: Object) => {
     ...compilationStats.assets.map(({ name, size }) => [name, formatSize(size)]),
   );
 
+  compilationStats.warnings.forEach(warning => {
+    logger.warn(warning);
+  });
   logger.print(table.toString(), '\n');
 };
 
