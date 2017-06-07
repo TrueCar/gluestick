523de96ae66c34068a23d2ac7f0c01b8f30ac75f
Paweł Trysła <pawel.trysla@gmail.com>
2017-06-07 15:25:48 +0200

    update build command test

--- packages/gluestick/src/commands/build/build.js
+++ packages/gluestick/src/commands/build/build.js
@@ -17,22 +17,24 @@ type CommandOptions = {
 }
 
 module.exports = (
-  { getOptions, getLogger, getContextConfig, getGluestickConfig, getPlugins }: CommandAPI, commandArgs: any[],
+  { getOptions, getLogger, getContextConfig, getGluestickConfig, getPlugins }: CommandAPI,
+  commandArgs: any[],
 ): void => {
   const options: CommandOptions = getOptions(commandArgs);
   const logger: Logger = getLogger();
   logger.clear();
   logger.printCommandInfo();
 
+  const compilationErrorHandler = (type: string) => error => {
+    logger.clear();
+    logger.fatal(`${type[0].toUpperCase()}${type.slice(1)} compilation failed`, error);
+  };
+
   // If neither server not client flag is passed
   // set them both to true to compile both client and server.
   if (!options.client && !options.server) {
     options.client = true;
-    // If app is passed by default disable server compilation.
-    if (options.app) {
-      logger.info('Disabling server compilation, you can enable it by passing `--server`');
-    }
-    options.server = !options.app;
+    options.server = true;
   }
 
   // If it's vendor compilation disable everything
@@ -63,46 +65,39 @@ module.exports = (
     webpackOptions.entryOrGroupToBuild = options.app;
   }
 
-  const plugins = getPlugins(logger);
-  const config: Object = options.client || options.server
-    ? getContextConfig(logger, webpackOptions)
-    : {
+  if (options.vendor) {
+    const plugins = getPlugins(logger);
+    const config: Object = {
       GSConfig: getGluestickConfig(logger, plugins),
       webpackConfig: {} // Don't need client and server configs, since we will add vendor config
-    }
-  const compilationErrorHandler = (type: string) => error => {
-    logger.clear();
-    logger.fatal(`${type[0].toUpperCase()}${type.slice(1)} compilation failed`, error);
-  };
-
-  if (options.vendor) {
+    };
     const vendorDllConfig = vendorDll.getConfig({ logger, config }, plugins);
+    config.webpackConfig.vendor = vendorDllConfig;
     if (!options.bailIfOk || !vendorDll.isValid({ logger, config }, vendorDllConfig)) {
       clearBuildDirectory(config.GSConfig, 'dlls');
-      config.webpackConfig.vendor = vendorDllConfig;
       compile({ logger, config }, options, 'vendor')
         .then(() => {
           vendorDll.injectValidationMetadata({ config }, vendorDllConfig);
         })
         .catch(compilationErrorHandler('vendor'));
     }
-  }
+  } else {
+    const config = getContextConfig(logger, webpackOptions);
+    let clientCompilation = Promise.resolve();
+    if (options.client) {
+      clearBuildDirectory(config.GSConfig, 'client');
+      clientCompilation = compile({ logger, config }, options, 'client')
+        .catch(compilationErrorHandler('client'));
+    }
 
-  let clientCompilation = Promise.resolve();
-  if (options.client) {
-    clearBuildDirectory(config.GSConfig, 'client');
-    clientCompilation
-      .then(() => compile({ logger, config }, options, 'client'))
-      .catch(compilationErrorHandler('client'));
-  }
-
-  if (options.server) {
-    clearBuildDirectory(config.GSConfig, 'server');
-    Promise.all([
-      compile({ logger, config }, options, 'server').catch(compilationErrorHandler('server')),
-      clientCompilation,
-    ]).then(() => {
-      return options.static ? getEntiresSnapshots({ config, logger }) : Promise.resolve();
-    });
+    if (options.server) {
+      clearBuildDirectory(config.GSConfig, 'server');
+      Promise.all([
+        compile({ logger, config }, options, 'server').catch(compilationErrorHandler('server')),
+        clientCompilation,
+      ]).then(() => {
+        return options.static ? getEntiresSnapshots({ config, logger }) : Promise.resolve();
+      });
+    }
   }
 };
