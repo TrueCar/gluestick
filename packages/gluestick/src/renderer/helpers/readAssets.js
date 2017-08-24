b6aa16d060d72c51daa94bbd1920b21138746200
Paweł Trysła <pawel.trysla@gmail.com>
2017-08-24 14:41:15 +0200

    fix flow error

--- packages/gluestick/src/renderer/helpers/readAssets.js
+++ packages/gluestick/src/renderer/helpers/readAssets.js
@@ -11,7 +11,9 @@ module.exports = (assetsPath: string): Promise<Object> => {
       fs.readFile(assetsPath, (error, assetsBuffer) => {
         if (error) {
           reject(
-            `Failed to read ${assetsPath} (${error.code}). Did you forgot to compile client bundle? ` +
+            `Failed to read ${assetsPath} ${error.code
+              ? `(${error.code})`
+              : ''}. Did you forgot to compile client bundle? ` +
               `Run 'gluestick build --client', then try again.`,
           );
         } else {
