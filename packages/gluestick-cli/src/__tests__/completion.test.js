1bfeb518f831f308fd41de31ad2804c772dc129c
Seth Battin <sbattin@truecar.com>
2017-09-02 17:44:41 -0500

    add real test and a helper

--- packages/gluestick-cli/src/__tests__/completion.test.js
+++ packages/gluestick-cli/src/__tests__/completion.test.js
@@ -2,9 +2,32 @@
 
 const completion = require("../completion").default;
 
+const cli = (line, cwd = ".") => {
+  const argvMimic = line.replace(/^gluestick/,"").trim().split(" ");
+  console.log("argv mimick:", argvMimic);
+  return completion(cwd, argvMimic);
+}
+
 describe("gluestick-cli/src/completion.js", () => {
   it("should be callable", () => {
     completion(".", []);
+  });
+
+  it("should return the commands", () => {
+    const options = cli("gluestick ");
+    expect(options).toEqual(expect.arrayContaining([
+      "build",
+      "destroy",
+      "dockerize",
+      "generate",
+      "new",
+      "reinstall-dev",
+      "reset-hard",
+      "start",
+      "start-client",
+      "start-server",
+      "test",
+    ]));
   });
 
 });
