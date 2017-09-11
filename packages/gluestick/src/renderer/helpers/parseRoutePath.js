e63d4f38074b8733b86c5ef306095a4a43099504
Paweł Trysła <pawel.trysla@gmail.com>
2017-09-11 10:32:39 +0200

    add flowtypes and test suite for parseRoutePath module

--- packages/gluestick/src/renderer/helpers/parseRoutePath.js
+++ packages/gluestick/src/renderer/helpers/parseRoutePath.js
@@ -1,10 +1,18 @@
+/* @flow */
+
 const pathToRegexp = require('path-to-regexp');
 
-module.exports = function parseRoutePath(routePath) {
+type PathRegexp = RegExp & {
+  fromRegex?: boolean,
+  keys?: string[],
+};
+
+module.exports = function parseRoutePath(routePath: string): PathRegexp {
   const match = /\/(.+)\/([gmiyu]*)$/.exec(routePath);
   if (match) {
     const [, pattern, modifiers] = match;
     const results = new RegExp(pattern, modifiers);
+    // $FlowIgnore
     results.fromRegex = true;
     return results;
   }
