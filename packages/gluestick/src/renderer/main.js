1e6b4a29eb739d8c0788b1476061aff818c2bac5
Todd Williams <toddsurfs@icloud.com>
2017-09-11 20:08:35 -0700

    move FlowIgnore

--- packages/gluestick/src/renderer/main.js
+++ packages/gluestick/src/renderer/main.js
@@ -16,6 +16,7 @@ import type {
 
 // Intentionally first require so things like require("newrelic") in
 // preInitHook get instantiated before anything else. This improves profiling
+// $FlowIgnore
 const projectHooks = require('gluestick-hooks').default;
 
 const path = require('path');
@@ -32,7 +33,6 @@ const entries = require('project-entries').default;
 const entriesConfig = require('project-entries-config');
 // $FlowIgnore
 const EntryWrapper = require('entry-wrapper').default;
-// $FlowIgnore
 const BodyWrapper = require('./components/Body').default;
 const reduxMiddlewares = require('redux-middlewares').default;
 // $FlowIgnore
