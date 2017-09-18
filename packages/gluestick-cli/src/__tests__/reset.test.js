180dbe3351020dfab5ea70817cab77af72df48f5
ferrannp <fnp.developer@gmail.com>
2017-09-18 14:36:04 +0200

    SELA-3277: Remove 'npm cache clean' for npm 5 and above.

---
+++ packages/gluestick-cli/src/__tests__/reset.test.js
@@ -0,0 +1,26 @@
+jest.mock('child_process', () => {
+  let count = 0;
+  return {
+    execSync: jest.fn(arg => {
+      if (arg === 'npm -v') {
+        const version = count === 0 ? '4.0.0' : '5.0.0';
+        count++;
+        return version;
+      }
+      return null;
+    }),
+  };
+});
+
+const reset = require('../reset');
+const { execSync } = require('child_process');
+
+it('calls npm cache clean only for versions previous to npm@5', () => {
+  // < 5.x.x
+  reset();
+  expect(execSync).toBeCalledWith(`npm cache clean`, { stdio: 'inherit' });
+  // >= 5.x.x
+  execSync.mockClear();
+  reset();
+  expect(execSync).not.toBeCalledWith(`npm cache clean`, { stdio: 'inherit' });
+});
--- packages/gluestick-cli/src/reset.js
+++ packages/gluestick-cli/src/reset.js
@@ -1,8 +1,18 @@
 const { execSync } = require('child_process');
 
+const _cleanCache = () => {
+  const npmVersion = execSync('npm -v').toString().trim();
+  const majorVersion = npmVersion.split('.')[0];
+  // As of npm@5, the npm cache self-heals from corruption issues and data extracted from the cache
+  // is guaranteed to be valid.
+  if (majorVersion < 5) {
+    execSync('npm cache clean', { stdio: 'inherit' });
+  }
+};
+
 module.exports = () => {
   execSync('rm -rf node_modules/gluestick', { stdio: 'inherit' });
   execSync('rm -rf build', { stdio: 'inherit' });
-  execSync('npm cache clean', { stdio: 'inherit' });
+  _cleanCache();
   execSync('gluestick reinstall-dev', { stdio: 'inherit' });
 };
