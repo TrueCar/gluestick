218987bffe64840410303e5f5d01330a60032a9b
Paweł Trysła <pawel.trysla@gmail.com>
2017-10-03 12:24:11 +0200

    use prop-types package instead of react

--- packages/gluestick/shared/containers/Root.js
+++ packages/gluestick/shared/containers/Root.js
@@ -1,6 +1,7 @@
 /* @flow */
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { applyRouterMiddleware, Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { useScroll } from 'react-router-scroll';
