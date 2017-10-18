b029b8904677c1ffb510dcda9806b273ac716f06
Paweł Trysła <pawel.trysla@gmail.com>
2017-10-18 11:21:21 +0200

    use node 8 in base docker image

--- scripts/docker/create-base-image.js
+++ scripts/docker/create-base-image.js
@@ -8,14 +8,14 @@ console.log(`Creating Docker base image for ${tag}`);
 
 module.exports = (spawn = crossSpawn) => {
   console.log('Building docker image...');
-  console.log(spawn('docker', [
+ spawn('docker', [
     'build',
     '-f', './scripts/docker/Dockerfile',
     '--force-rm=true',
     '-t', tag,
     '--build-arg', `GLUESTICK_VERSION=${version}`,
     '.',
-  ], { stdio: 'inherit', env: Object.assign({}, process.env) }));
+  ], { stdio: 'inherit', env: Object.assign({}, process.env) });
 
   console.log('Pushing image to Docker Hub...');
   spawn('docker', ['push', tag], { stdio: 'inherit', env: Object.assign({}, process.env) });
