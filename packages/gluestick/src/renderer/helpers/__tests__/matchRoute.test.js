400a607569ef6c9fde177cc7b1b51807329d5846
Henry Tran <htran@truecar.com>
2017-10-27 17:08:14 -0700

    Lint

--- packages/gluestick/src/renderer/helpers/__tests__/matchRoute.test.js
+++ packages/gluestick/src/renderer/helpers/__tests__/matchRoute.test.js
@@ -84,7 +84,6 @@ describe('renerer/helpers/matchRoute', () => {
 
   describe('when the route has an invalid queryParam', () => {
     it('should forward to the promise `catch` with an error', () => {
-
       const mockMatch = jest.fn();
       jest.mock('react-router', () => ({
         match: mockMatch,
