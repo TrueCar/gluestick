63c3a58e61f85d72dda51408d8c36eb5532bf155
Paweł Trysła <pawel.trysla@gmail.com>
2017-11-08 13:15:30 +0100

    update test

--- packages/gluestick/src/renderer/helpers/__tests__/getAssetsLoader.test.js
+++ packages/gluestick/src/renderer/helpers/__tests__/getAssetsLoader.test.js
@@ -2,9 +2,7 @@ const getAssetsLoader = require('../getAssetsLoader');
 
 describe('renderer/helpers/getAssetsLoader', () => {
   it('should return code for assets loading in browser', () => {
-    const snippet = getAssetsLoader({}, 'entryBundle.js', 'vendorBundle.js', {
-      GSConfig: { protocol: '', host: '', ports: { server: '0000' } },
-    });
+    const snippet = getAssetsLoader({}, 'entryBundle.js', 'vendorBundle.js');
     expect(snippet.length).toBeGreaterThan(0);
     expect(snippet).toContain(
       "document.addEventListener('DOMContentLoaded', loadVendorThenEntry)",
@@ -20,9 +18,6 @@ describe('renderer/helpers/getAssetsLoader', () => {
       },
       'entryBundle.js',
       'vendorBundle.js',
-      {
-        GSConfig: { protocol: '', host: '', ports: { server: '0000' } },
-      },
     );
     expect(snippet.length).toBeGreaterThan(0);
     expect(snippet).toContain(
