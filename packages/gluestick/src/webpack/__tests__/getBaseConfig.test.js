13d8a5b87ee395d9e6ec7c154339845647898031
Paweł Trysła <pawel.trysla@gmail.com>
2017-12-14 15:28:16 +0100

    add getBaseConfig test suite

---
+++ packages/gluestick/src/webpack/__tests__/getBaseConfig.test.js
@@ -0,0 +1,57 @@
+jest.mock('../utils/buildClientEntrypoints.js');
+jest.mock('../utils/buildServerEntrypoints.js');
+jest.mock('../utils/getAliasesForApps.js');
+
+const getBaseConfig = require('../getBaseConfig');
+const buildClientEntrypoints = require('../utils/buildClientEntrypoints');
+const buildServerEntrypoints = require('../utils/buildServerEntrypoints');
+
+const loggerMock = require('../../__tests__/mocks/context').commandApi.getLogger();
+
+describe('webpack/getBaseConfig', () => {
+  it('should create configs abd build entrypoints', () => {
+    buildClientEntrypoints.mockImplementationOnce(() => ({
+      main: 'main.js',
+    }));
+    buildServerEntrypoints.mockClear();
+
+    const { client, server } = getBaseConfig(
+      {
+        entries: {},
+        noProgress: false,
+        clientPlugins: [],
+        serverPlugins: [],
+        skipClientEntryGeneration: false,
+        skipServerEntryGeneration: false,
+      },
+      { logger: loggerMock },
+    );
+
+    expect(client).toBeDefined();
+    expect(server).toBeDefined();
+    expect(buildClientEntrypoints).toHaveBeenCalled();
+    expect(buildServerEntrypoints).toHaveBeenCalled();
+  });
+
+  it('should create configs without building entrypoints', () => {
+    buildClientEntrypoints.mockClear();
+    buildServerEntrypoints.mockClear();
+
+    const { client, server } = getBaseConfig(
+      {
+        entries: {},
+        noProgress: false,
+        clientPlugins: [],
+        serverPlugins: [],
+        skipClientEntryGeneration: true,
+        skipServerEntryGeneration: true,
+      },
+      { logger: loggerMock },
+    );
+
+    expect(client).toBeDefined();
+    expect(server).toBeDefined();
+    expect(buildClientEntrypoints).not.toHaveBeenCalled();
+    expect(buildServerEntrypoints).not.toHaveBeenCalled();
+  });
+});
