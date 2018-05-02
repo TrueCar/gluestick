88ee10cc7b4698cc073ef18417b8de7c5d3ce692
David Edmondson <dedmondson@truecar.com>
2018-05-02 09:50:58 -0700

    Separate server config from shared.

--- packages/gluestick/src/config/webpack/webpack.config.server.js
+++ packages/gluestick/src/config/webpack/webpack.config.server.js
@@ -4,10 +4,14 @@ import type { WebpackConfig, GSConfig, Logger } from '../../types';
 
 const webpack = require('webpack');
 const path = require('path');
-const deepClone = require('clone');
 const nodeExternals = require('webpack-node-externals');
+const autoprefixer = require('autoprefixer');
+const cssCustomProperties = require('postcss-custom-properties');
+const postcssCalc = require('postcss-calc');
+
 const progressHandler = require('./progressHandler');
 const buildServerEntries = require('./buildServerEntries');
+const getAliasesForApps = require('./getAliasesForApps');
 
 module.exports = (
   logger: Logger,
@@ -21,6 +25,25 @@ module.exports = (
     noProgress,
   }: { skipEntryGeneration: boolean, noProgress: boolean } = {},
 ): WebpackConfig => {
+  const {
+    buildAssetsPath,
+    assetsPath,
+    sourcePath,
+    appsPath,
+    sharedPath,
+    configPath,
+    nodeModulesPath,
+  } = gluestickConfig;
+  const appRoot: string = process.cwd();
+
+  const outputPath: string = path.resolve(appRoot, buildAssetsPath);
+
+  const ouputFileName = path.basename(
+    settings.server.output,
+    path.extname(settings.server.output),
+  );
+
+  // TODO side effect in configuration! figure out how to get rid of this thing!
   if (!skipEntryGeneration) {
     buildServerEntries(
       gluestickConfig,
@@ -29,84 +52,158 @@ module.exports = (
       runtimeAndServerPlugins,
     );
   }
-  const config = deepClone(configuration);
-  const ouputFileName = path.basename(
-    settings.server.output,
-    path.extname(settings.server.output),
-  );
 
-  config.entry = {
-    [ouputFileName]: settings.server.input,
-  };
-  config.output = {
-    path: path.dirname(settings.server.output),
-    filename: '[name].js',
-    chunkFilename: '[name].js',
-    libraryTarget: 'commonjs2',
-    pathinfo: true,
-  };
-  // Disable warning for `getVersion` function from `cli/helpers.js`, which has dynamic require,
-  // but it's not used by server.
-  config.module.noParse = [/cli\/helpers/];
-  config.module.rules[1].use = ['ignore-loader'];
-  config.module.rules[2].use = ['ignore-loader'];
-  config.module.rules[3].use[0].options.emitFile = false;
-  config.module.rules[4].use[0].options.emitFile = false;
-  config.resolve.alias['project-entries'] = path.join(
-    process.cwd(),
-    gluestickConfig.serverEntriesPath,
-  );
-  config.resolve.alias['project-entries-config'] = path.join(
-    process.cwd(),
-    gluestickConfig.entriesPath,
-  );
-  config.resolve.alias['entry-wrapper'] = path.join(
-    process.cwd(),
-    gluestickConfig.entryWrapperPath,
-  );
-  config.resolve.alias['gluestick-hooks'] = path.join(
-    process.cwd(),
-    gluestickConfig.hooksPath,
-  );
-  config.resolve.alias['redux-middlewares'] = path.join(
-    process.cwd(),
-    gluestickConfig.reduxMiddlewares,
-  );
-  config.resolve.alias['plugins-config-path'] =
-    gluestickConfig.pluginsConfigPath;
-  config.resolve.alias['application-config'] = path.join(
-    config.resolve.alias.root,
-    gluestickConfig.sourcePath,
-    gluestickConfig.configPath,
-    gluestickConfig.applicationConfigPath,
-  );
-  if (!noProgress) {
-    config.plugins.push(progressHandler(logger, 'server'));
-  }
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    }),
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1,
-    }),
-  );
-  config.node.__dirname = false;
-  config.node.__filename = false;
-  config.target = 'node';
-  config.devtool = 'source-map';
-
-  // "externals" speeds up server builds by not bundling modules that could be imported,
-  // but certain server/client packages with global caches need to be bundled.
-  config.externals = [
-    nodeExternals({
-      whitelist: [
-        /react-universal-component/,
-        /webpack-flush-chunks/,
-        /universal-import/,
+  return {
+    context: appRoot,
+    entry: {
+      [ouputFileName]: settings.server.input,
+    },
+    resolve: {
+      extensions: ['.js', '.css', '.json'],
+      alias: {
+        ...getAliasesForApps(gluestickConfig),
+        root: appRoot,
+        src: path.join(appRoot, sourcePath),
+        assets: path.join(appRoot, assetsPath),
+        shared: path.join(appRoot, sourcePath, sharedPath),
+        config: path.join(appRoot, sourcePath, configPath),
+        apps: path.join(appRoot, sourcePath, appsPath),
+        compiled: path.join(appRoot, nodeModulesPath),
+        'project-entries': path.join(
+          appRoot,
+          gluestickConfig.serverEntriesPath,
+        ),
+        'project-entries-config': path.join(
+          appRoot,
+          gluestickConfig.entriesPath,
+        ),
+        'entry-wrapper': path.join(appRoot, gluestickConfig.entryWrapperPath),
+        'gluestick-hooks': path.join(appRoot, gluestickConfig.hooksPath),
+        'redux-middlewares': path.join(
+          appRoot,
+          gluestickConfig.reduxMiddlewares,
+        ),
+        'plugins-config-path': gluestickConfig.pluginsConfigPath,
+        'application-config': path.join(
+          appRoot,
+          gluestickConfig.sourcePath,
+          gluestickConfig.configPath,
+          gluestickConfig.applicationConfigPath,
+        ),
+      },
+    },
+    output: {
+      path: path.dirname(settings.server.output),
+      filename: '[name].js',
+      chunkFilename: '[name].js',
+      libraryTarget: 'commonjs2',
+      pathinfo: true,
+    },
+    module: {
+      noParse: [/cli\/helpers/],
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: new RegExp(
+            `(node_modules/(?!gluestick))|(${buildAssetsPath})`,
+          ),
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                plugins: ['transform-flow-strip-types'],
+                presets: ['es2015', 'react', 'stage-0'],
+                babelrc: false,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(scss)$/,
+          use: ['ignore-loader'],
+        },
+        {
+          test: /\.(css)$/,
+          use: ['ignore-loader'],
+        },
+        {
+          test: /\.(png|jpg|gif|ico|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name]-[hash].[ext]',
+                emitFile: false,
+              },
+            },
+            {
+              loader: 'image-webpack-loader',
+              // Workaround https://github.com/tcoopman/image-webpack-loader/issues/88#issuecomment-289454242
+              options: {},
+            },
+          ],
+        },
+        {
+          test: /\.(woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name]-[hash].[ext]',
+                emitFile: false,
+              },
+            },
+          ],
+        },
       ],
-    }),
-  ];
+    },
 
-  return config;
+    plugins: [
+      new webpack.LoaderOptionsPlugin({
+        test: /\.(scss|css)$/,
+        options: {
+          // A temporary workaround for `scss-loader`
+          // https://github.com/jtangelder/sass-loader/issues/298
+          output: {
+            path: outputPath,
+          },
+
+          postcss: [
+            autoprefixer({ browsers: 'last 2 version' }),
+            cssCustomProperties(),
+            postcssCalc(),
+          ],
+
+          // A temporary workaround for `css-loader`.
+          // Can also supply `query.context` parameter.
+          context: appRoot,
+        },
+      }),
+      !noProgress && progressHandler(logger, 'server'),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      }),
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      }),
+    ].filter(Boolean),
+    node: {
+      net: 'empty',
+      __dirname: false,
+      __filename: false,
+    },
+    target: 'node',
+    devtool: 'source-map',
+    // "externals" speeds up server builds by not bundling modules that could be imported,
+    // but certain server/client packages with global caches need to be bundled.
+    externals: [
+      nodeExternals({
+        whitelist: [
+          /react-universal-component/,
+          /webpack-flush-chunks/,
+          /universal-import/,
+        ],
+      }),
+    ],
+  };
 };
