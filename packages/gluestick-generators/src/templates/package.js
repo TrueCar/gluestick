d36a6e3d57c568f401eb0d4c9d01d34d12bcdb6f
Christine Brass <cybrass@gmail.com>
2018-09-28 05:18:28 -0700

    Fix enzyme-react-adapter dependency version

--- packages/gluestick-generators/src/templates/package.js
+++ packages/gluestick-generators/src/templates/package.js
@@ -65,7 +65,7 @@ const templatePackage = createTemplate`
     "babel-jest": "18.0.0",
     "babel-plugin-react-transform": "2.0.2",
     "enzyme": "3.3.0",
-    "enzyme-adapter-react-16": "1.5.1",
+    "enzyme-adapter-react-16": "1.5.0",
     "eslint": "3.14.1",
     "eslint-plugin-react": "6.9.0",
     "flow-bin": "0.45.0",
